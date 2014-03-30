40790e1fa23c5e17aca7531cf7c476ddb1d68d5e
simone.bna <simone.bna@unibo.it>
2014-03-30 17:50:50 +0200

    add enums

---
+++ include/enums/DirichletBCTypeEnum.hpp
@@ -0,0 +1,9 @@
+#ifndef __dirichletBCtypeenum_h__
+#define __dirichletBCtypeenum_h__
+
+enum DirichletBCType {
+  PENALTY=0,
+  ELIMINATION
+};
+
+#endif
---
+++ include/enums/MgSmootherEnum.hpp
@@ -0,0 +1,9 @@
+#ifndef __mgsmootherenum_h__
+#define __mgsmootherenum_h__
+
+enum MgSmoother {
+  VANKA_SMOOTHER=0,
+  GMRES_SMOOTHER
+};
+
+#endif
