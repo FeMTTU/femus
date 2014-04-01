a30150e892250ecc7104b6012f130631412c2b1c
simone.bna <simone.bna@unibo.it>
2014-04-01 21:01:24 +0200

    add output classes and reused the same piece of code for updatebdc and generatebdc

---
+++ include/equations/GMVOutput.hpp
@@ -0,0 +1,45 @@
+/*=========================================================================
+
+ Program: FEMUS
+ Module: GMVOutput
+ Authors: Eugenio Aulisa, Simone Bnà
+ 
+ Copyright (c) FEMTTU
+ All rights reserved. 
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __gmvoutput_h_
+#define __gmvoutput_h_
+
+//----------------------------------------------------------------------------
+// includes :
+//----------------------------------------------------------------------------
+#include "Output.hpp"
+
+//------------------------------------------------------------------------------
+// Forward declarations
+//------------------------------------------------------------------------------
+class MultiLevelProblem;
+
+
+class GMVOutput : public Output {
+
+public:
+
+  /** Constructor. */
+  GMVOutput(MultiLevelProblem& ml_probl);
+
+  /** Destructor */
+  virtual ~GMVOutput();
+  
+  /** write output function */
+  virtual void write_system_solutions();
+  
+};
+
+#endif
