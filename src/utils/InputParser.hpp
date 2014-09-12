6e71643a7ff5fe00c9024e770977ac19c78c06b0
simone.bna <simone.bna@unibo.it>
2014-09-12 13:22:38 +0200

    Add comments, reduced duplicated code and automatic concrete class instantation by file extension

--- src/utils/InputParser.hpp
+++ src/utils/InputParser.hpp
@@ -34,9 +34,19 @@ PURPOSE.  See the above copyright notice for more information.
 //------------------------------------------------------------------------------
 
 namespace femus {
-  
+
 /**
-* This class is an abstract class for input parsing.
+* This class is an abstract class for input parsing. It implements the Factory Method pattern. Using the static \p build function
+* you can istantiate the concrete classes HTMLInputParser (not yet implemented) or JsonInputParser.
+* 
+* How to use it:
+* the path of the variable in the json input file is expressed as a sequence of words separated by a point
+* e.g. :
+* "mesh.first.type.box"
+* 
+* use the getValue function to read and get the value
+* use the getSize function to get the size of a vector
+* use the isTrue function to check if the basepath.type value in the file is equal or not to the type value you ask to   
 */
 
 class InputParser {
@@ -45,48 +55,62 @@ public:
 
     /** Constructor */
     InputParser(const std::string& infile);
-    
+
     /** Destructor */
     virtual ~InputParser();
-    
-    /** Builds a \p a using the b in \p c */
-    static std::auto_ptr<InputParser> build(const std::string& infile, const int format);
-    
+
+    /** Build the concrete class using the Factory Method pattern */
+    static std::auto_ptr<InputParser> build(const std::string& infile);
+
+    /** Who I am? */
     virtual void printInfo() = 0;
-    
+
+    /** return true if the argument type is equal to the basepath.type value in the json input file */
     virtual const bool isTrue(const std::string& basepath, const std::string& type ) = 0;
-    
+
+    /** return the size of the vector at the \p fullpath position in the json input file */
     virtual const unsigned int getSize(const std::string& fullpath) = 0;
-    
+
+    /** return the int value at the \p fullpath position in the json input file */
     virtual const int getValue(const std::string& fullpath, const int defaultvalue) = 0;
-    
+
+    /** return the double value at the \p fullpath position in the json input file */
     virtual const double getValue(const std::string& fullpath, const double defaultvalue) = 0;
-    
+
+    /** return the ElemType value at the \p fullpath position in the json input file */
     virtual const ElemType getValue(const std::string& fullpath, const ElemType defaultvalue) = 0;
-    
+
+    /** return the FEOrder value at the \p fullpath position in the json input file */
     virtual const FEOrder getValue(const std::string& fullpath, const FEOrder defaultvalue) = 0;
-    
+
+    /** return the string value at the \p fullpath position in the json input file */
     virtual const std::string getValue(const std::string& fullpath, const std::string defaultvalue) = 0;
-    
+
+    /** return the MgType value at the \p fullpath position in the json input file */
     virtual const MgType getValue(const std::string& fullpath, const MgType defaultvalue) = 0;
-    
-    virtual const std::string getValueFromArray(const std::string& basepath, const unsigned int index, 
-					        const std::string entry, const std::string defaultvalue) = 0;
-					   
-    virtual const BDCType getValueFromArray(const std::string& basepath, const unsigned int index, 
- 					 const std::string entry, const BDCType defaultvalue) = 0;
-					 
+
+    /** return the string value at the \p fullpath position in the json input file */
+    virtual const std::string getValueFromArray(const std::string& basepath, const unsigned int index,
+            const std::string entry, const std::string defaultvalue) = 0;
+
+    /** return the BDCType value at the \p fullpath position in the json input file */
+    virtual const BDCType getValueFromArray(const std::string& basepath, const unsigned int index,
+                                            const std::string entry, const BDCType defaultvalue) = 0;
+
 protected:
-  
-    std::string readInputTestFile(const char *path);    
-    
-    unsigned int howManyWords(const std::string& input) const; 
-    
+
+    /** return a string that stands for the input file for the Jsoncpp library*/
+    std::string readInputTestFile(const char *path);
+
+    /** return how many words are in the input string */
+    unsigned int howManyWords(const std::string& input) const;
+
+    /** return the word in the input string at the \p iter position */
     const std::string extractWord(const std::string& input, const int iter) const;
-    
-    std::string _infile;
-    
-    
+
+    std::string _infile; //< the input file
+
+
 };
 
 } //end namespace femus
--- src/utils/JsonInputParser.hpp
+++ src/utils/JsonInputParser.hpp
@@ -21,7 +21,7 @@ PURPOSE.  See the above copyright notice for more information.
 //----------------------------------------------------------------------------
 #include "InputParser.hpp"
 #include <json/json.h>
-#include <json/value.h>  
+#include <json/value.h>
 
 #include "iostream"
 
@@ -41,39 +41,55 @@ public:
 
     /** Constructor */
     JsonInputParser(const std::string& infile);
-    
+
     /** Destructor. */
     ~JsonInputParser ();
 
-    void printInfo() {std::cout << "I'm Json" << std::endl;};
-    
+    void printInfo() {
+        std::cout << "I'm Json" << std::endl;
+    };
+
+    /** See father class */
     const bool isTrue(const std::string& base, const std::string& type);
-    
+
+    /** See father class */
     const unsigned int getSize(const std::string& base);
-    
+
+    /** See father class */
     const int getValue(const std::string& fullpath, const int defaultvalue);
-    
+
+    /** See father class */
     const double getValue(const std::string& fullpath, const double defaultvalue);
-    
+
+    /** See father class */
     const ElemType getValue(const std::string& fullpath, const ElemType defaultvalue);
-    
+
+    /** See father class */
     const std::string getValue(const std::string& fullpath, const std::string defaultvalue);
-    
+
+    /** See father class */
     const FEOrder getValue(const std::string& fullpath, const FEOrder defaultvalue);
-    
+
+    /** See father class */
     const MgType getValue(const std::string& fullpath, const MgType defaultvalue);
-    
-    const std::string getValueFromArray(const std::string& basepath, const unsigned int index, 
-					const std::string entry, const std::string defaultvalue);
-    
-    const BDCType getValueFromArray(const std::string& basepath, const unsigned int index, 
-			         const std::string entry, const BDCType defaultvalue);
-    
-private:    
-    
-  Json::Value _root;   //< will contains the root value after parsing.
-  Json::Reader _reader;
-    
+
+    /** See father class */
+    const std::string getValueFromArray(const std::string& basepath, const unsigned int index,
+                                        const std::string entry, const std::string defaultvalue);
+
+    /** See father class */
+    const BDCType getValueFromArray(const std::string& basepath, const unsigned int index,
+                                    const std::string entry, const BDCType defaultvalue);
+
+private:
+
+    /** To be added */
+    template <typename T_sys>
+    const Json::Value getJsonValue(const std::string& basepath, const T_sys defaultvalue);
+
+    Json::Value _root;     //< will contains the root value after parsing.
+    Json::Reader _reader;  //< will contain the json file reader
+
 };
 
 } //end namespace femus
