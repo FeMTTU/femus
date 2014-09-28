11b6c79e71aafa6f5c213c7e2d3c165e162b892d
Eugenio Aulisa <eugenio.aulisa@ttu.edu>
2014-09-28 07:19:53 -0500

    bathe strange

--- applications/FSIApps/include/IncompressibleFSIAssembly.hpp
+++ applications/FSIApps/include/IncompressibleFSIAssembly.hpp
@@ -40,15 +40,19 @@ namespace femus {
     vector <bool> solidmark;
   
     vector <double > phi;
+    vector <double > phi2;
     vector <double > phi_hat;
   
     vector <double> gradphi;
+    vector <double> gradphi2;
     vector <double> gradphi_hat;
   
     metis_node1.reserve(max_size);
     metis_node2.reserve(max_size);
     solidmark.reserve(max_size);
     phi.reserve(max_size);
+    phi2.resize(9);
+    gradphi2.resize(27);
     phi_hat.reserve(max_size);
     gradphi.reserve(max_size*dim);
     gradphi_hat.reserve(max_size*dim);
@@ -61,11 +65,13 @@ namespace femus {
   
     vector <vector < double> > vx(dim);
     vector <vector < double> > vx_face(dim);
+    vector <vector < double> > vx_face2(dim);
     vector <vector < double> > vx_hat(dim);
   
     for(int i=0;i<dim;i++){
       vx[i].reserve(max_size);
       vx_face[i].resize(9);
+      vx_face2[i].resize(9);
       vx_hat[i].reserve(max_size);
     }
    
@@ -320,28 +326,46 @@ namespace femus {
 		  const unsigned FELT[6][2]= {{3,3},{4,4},{3,4},{5,5},{5,5},{6,6}};
 		  unsigned felt = FELT[kelt][jface < mymsh->el->GetElementFaceNumber(kel,0)];
 		    
+		   //std::cout<<kel<<" "<<jface<<" "<<tau<<"\n";
+		  
 		  for(unsigned i=0; i<nve; i++) {
 		    unsigned inode=mymsh->el->GetFaceVertexIndex(kel,jface,i)-1u;
 		    unsigned inode_coord_metis=mymsh->GetMetisDof(inode,2);
-
+		    //std::cout<<i<<" -> "<<inode<<" -> "<<inode_coord_metis<<std::endl; 
 		    for(unsigned ivar=0; ivar<dim; ivar++) {
 		      vx_face[ivar][i]=(*mymsh->_coordinate->_Sol[ivar])(inode_coord_metis);
+		      vx_face2[(ivar+2)%dim][i]=(*mymsh->_coordinate->_Sol[ivar])(inode_coord_metis);
 		      //std::cout<<vx_face[ivar][i]<<" ";
 		    }
 		    //std::cout<<std::endl;
 		  }
-		  //std::cout<<kel<<" "<<face<<" "<<tau<<"\n";
+		  
 		  for(unsigned igs=0; igs < ml_prob._ml_msh->_type_elem[felt][order_ind2]->GetGaussPointNumber(); igs++) {
-		    (ml_prob._ml_msh->_type_elem[felt][order_ind2]->*ml_prob._ml_msh->_type_elem[felt][order_ind2]->Jacobian_sur_ptr)(vx_face,igs,Weight,phi,gradphi,normal);
+		    (ml_prob._ml_msh->_type_elem[felt][order_ind2]->*(ml_prob._ml_msh->_type_elem[felt][order_ind2])->Jacobian_sur_ptr)(vx_face,igs,Weight,phi,gradphi,normal);
+		    //std::cout<<Weight<<" "<<normal[0];
+		    //for(unsigned i=0; i<nve; i++) 
+		     // std::cout<<phi[i]<<" ";
+		   
+		    double Weight2;
+		    (ml_prob._ml_msh->_type_elem[3][order_ind2]->*(ml_prob._ml_msh->_type_elem[3][order_ind2])->Jacobian_ptr)(vx_face2,igs,Weight2,phi2,gradphi2);
+		    // std::cout<<std::endl<<Weight<<" ";		     
+		      //for(unsigned i=0; i<nve; i++)
+			//std::cout<<phi[i]<<" ";
+		      //std::cout<<std::endl;
 		    // *** phi_i loop ***
 		    double bdintegral1=0;
 		    for(unsigned i=0; i<nve; i++) {
-		      double bdintegral = phi[i]*tau/rhof*Weight;
+		      double bdintegral = - phi[i]*tau/rhof*Weight;
 		      bdintegral1 += phi[i];///rhof*Weight;
 		      unsigned int ilocalnode = mymsh->el->GetLocalFaceVertexIndex(kel, jface, i);
-		      Rhs[indexVAR[dim]][ilocalnode] += bdintegral;
+		      //std::cout<<ilocalnode<<"->"<<myel->GetElementVertexIndex(kel,ilocalnode)-1u<<" ";
+		      
+		      Rhs[indexVAR[dim]][ilocalnode]   += bdintegral*normal[0];
+		      Rhs[indexVAR[dim+1]][ilocalnode] += bdintegral*normal[1];
+		      Rhs[indexVAR[dim+2]][ilocalnode] += bdintegral*normal[2];
 		    }
 		    //std::cout<<Weight<<"     ";
+		    //std::cout<<std::endl;
 		  }
 		  //std::cout<<std::endl;
 		}
@@ -733,7 +757,7 @@ namespace femus {
 			  // -(v_n+1,eta)
 			  B[indexVAR[idim]][indexVAR[dim+idim]][i*nve+j] -= (*(fi))*(*(fj))*Weight_hat;
 			  //  (u_n+1,eta)
-			  B[indexVAR[idim]][indexVAR[idim]][i*nve+j] 	 +=  (*(fi))*(*(fj))*Weight_hat;
+			  B[indexVAR[idim]][indexVAR[idim]][i*nve+j] 	 += (*(fi))*(*(fj))*Weight_hat;
 			}
 			
 			if(1 == solid_model){
