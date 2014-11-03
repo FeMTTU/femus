1c61662162137323fb77e699c8ffe012af1c2032
eaulisa <eugenio.aulisa@ttu.edu>
2014-11-03 16:31:17 -0600

    basis class almost ready

--- src/fe/Tetrahedral.cpp
+++ src/fe/Tetrahedral.cpp
@@ -28,36 +28,9 @@ namespace femus {
 //************************************************************
 
 double tet0::eval_phi(const int *I,const double* x) const {
-  return tet_0(x[0],x[1],x[2],I[0],I[1],I[2]);
+  return 1;
 }
 
-double tet0::eval_dphidx(const int *I,const double* x) const {
-  return dtet_0dx(x[0],x[1],x[2],I[0],I[1],I[2]);
-}
-
-double tet0::eval_dphidy(const int *I,const double* x) const {
-  return dtet_0dy(x[0],x[1],x[2],I[0],I[1],I[2]);
-}
-
-double tet0::eval_dphidz(const int *I,const double* x) const {
-  return dtet_0dz(x[0],x[1],x[2],I[0],I[1],I[2]);
-}
-
-double tet0::tet_0(const double& x, const double& y, const double& z, const int & i,const int & j,const int &k) const {
-  return 1.;
-}
-
-double tet0::dtet_0dx(const double& x, const double& y, const double& z, const int & i,const int & j,const int &k) const {
-  return 0.;
-}
-
-double tet0::dtet_0dy(const double& x, const double& y, const double& z, const int & i,const int & j,const int &k) const {
-  return 0.;
-}
-
-double tet0::dtet_0dz(const double& x, const double& y, const double& z, const int & i,const int & j,const int &k) const {
-  return 0.;
-}
 //************************************************************
 
 double tet1::eval_phi(const int *I,const double* x) const {
@@ -94,256 +67,346 @@ double tet1::dtet_1dz(const double& x, const double& y, const double& z, const i
 
 //************************************************************
 
-double  tet2::eval_phi(const int *I,const double* x) const {
-  return tet_2(x[0],x[1],x[2],I[0],I[1],I[2]);
+double  tet2::eval_phi(const int *I,const double* X) const {
+  const double x=X[0];   const double y=X[1];   const double z=X[2];
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  
+  double t=1.-(x+y+z);
+ 
+  return
+    !i 	   *( !j     *( !k*t*(2.*t-1.) + !(k-1)*4.*z*t + !(k-2)*(-z+2.*z*z) )+
+	      !(j-1) *( !k*4.*y*t      + !(k-1)*4.*y*z			   )+
+	      !(j-2) *( !k*(-y+2.*y*y) 					   ) )+
+    !(i-1) *( !j     *( !k*4.*x*t      + !(k-1)*4.*x*z			   )+
+	     !(j-1)  *( !k*4.*x*y					   ) )+
+    !(i-2) *( !j     *( !k*(-x+2.*x*x)					   ) );
+	
+
+//   double f=0.;
+//   double t=x+y+z;
+//   
+//   switch (i) {
+//   case 0:
+//     switch (j) {
+//     case 0:
+//       switch (k) {
+//       case 0:
+//         f=t*(2.*t-1.);
+//         break;
+//       case 1:
+//         f=4.*z*t;
+//         break;
+//       case 2:
+//         f=(-z+2.*z*z);
+//         break;
+//       }
+//       break;
+//     case 1:
+//       switch (k) {
+//       case 0:
+//         f=4.*y*t;
+//         break;
+//       case 1:
+//         f=4.*y*z;
+//         break;
+//       }
+//       break;
+//     case 2:
+//       f=(-y+2.*y*y);
+//       break;
+//     }
+//     break;
+//   case 1:
+//     switch (j) {
+//     case 0:
+//       switch (k) {
+//       case 0:
+//         f=4.*x*t;
+//         break;
+//       case 1:
+//         f=4.*x*z;
+//         break;
+//       }
+//       break;
+//     case 1:
+//       f=4.*x*y;
+//       break;
+//     }
+//     break;
+//   case 2:
+//     f=(-x+2.*x*x);
+//     break;
+//   }
+//   return f;
 }
 
-double  tet2::eval_dphidx(const int *I,const double* x) const {
-  return dtet_2dx(x[0],x[1],x[2],I[0],I[1],I[2]);
+double  tet2::eval_dphidx(const int *I,const double* X) const {
+  const double x=X[0];   const double y=X[1];   const double z=X[2];
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  
+  double t=1.-(x+y+z);
+  
+   return
+    !i 	  *( !j     *( !k*(-4.*t+1.) + !(k-1)*(-4.)*z )+
+	     !(j-1) *( !k*(-4.)*y 		      ) )+
+    !(i-1)*( !j     *( !k*4.*(t-x)   + !(k-1)*4.*z    )+
+	     !(j-1) *( !k*4.*y			      ) )+
+    !(i-2)*( !j     *( !k*(-1.+4.*x)		      ) );
+  
+  
+  
+//   double f=0.;
+//   double t=x+y+z;    
+//   switch (i) {
+//   case 0:
+//     switch (j) {
+//     case 0:
+//       switch (k) {
+//       case 0:
+//         f=(-3.+4.*t);
+//         break;
+//       case 1:
+//         f=-4.*z;
+//         break;
+//       case 2:
+//         f=0.;
+//         break;
+//       }
+//       break;
+//     case 1:
+//       switch (k) {
+//       case 0:
+//         f=-4.*y;
+//         break;
+//       case 1:
+//         f=0.;
+//         break;
+//       }
+//       break;
+//     case 2:
+//       f=0;
+//       break;
+//     }
+//     break;
+//   case 1:
+//     switch (j) {
+//     case 0:
+//       switch (k) {
+//       case 0:
+//         f=4.*(1.-t-x);
+//         break;
+//       case 1:
+//         f=4.*z;
+//         break;
+//       }
+//       break;
+//     case 1:
+//       f=4.*y;
+//       break;
+//     }
+//     break;
+//   case 2:
+//     f=(-1.+4.*x);
+//     break;
+//   }
+//   return f;
+  
 }
 
-double  tet2::eval_dphidy(const int *I,const double* x) const {
-  return dtet_2dy(x[0],x[1],x[2],I[0],I[1],I[2]);
-}
-
-
-double  tet2::eval_dphidz(const int *I,const double* x) const {
-  return dtet_2dz(x[0],x[1],x[2],I[0],I[1],I[2]);
-}
-
-double tet2::tet_2(const double& x, const double& y, const double &z, const int& i,const int& j, const int & k) const {
-  double f=0.;
-  double t=x+y+z;
-  switch (i) {
-  case 0:
-    switch (j) {
-    case 0:
-      switch (k) {
-      case 0:
-        f=(1.-t)*(1.-2.*t);
-        break;
-      case 1:
-        f=4.*z*(1.-t);
-        break;
-      case 2:
-        f=(-z+2.*z*z);
-        break;
-      }
-      break;
-    case 1:
-      switch (k) {
-      case 0:
-        f=4.*y*(1.-t);
-        break;
-      case 1:
-        f=4.*y*z;
-        break;
-      }
-      break;
-    case 2:
-      f=(-y+2.*y*y);
-      break;
-    }
-    break;
-  case 1:
-    switch (j) {
-    case 0:
-      switch (k) {
-      case 0:
-        f=4.*x*(1.-t);
-        break;
-      case 1:
-        f=4.*x*z;
-        break;
-      }
-      break;
-    case 1:
-      f=4.*x*y;
-      break;
-    }
-    break;
-  case 2:
-    f=(-x+2.*x*x);
-    break;
-  }
-  return f;
-}
-
-double tet2::dtet_2dx(const double& x, const double& y, const double &z, const int& i,const int& j, const int & k) const {
-  double f=0.;
-  double t=x+y+z;
-  switch (i) {
-  case 0:
-    switch (j) {
-    case 0:
-      switch (k) {
-      case 0:
-        f=(-3.+4.*t);
-        break;
-      case 1:
-        f=-4.*z;
-        break;
-      case 2:
-        f=0.;
-        break;
-      }
-      break;
-    case 1:
-      switch (k) {
-      case 0:
-        f=-4.*y;
-        break;
-      case 1:
-        f=0.;
-        break;
-      }
-      break;
-    case 2:
-      f=0;
-      break;
-    }
-    break;
-  case 1:
-    switch (j) {
-    case 0:
-      switch (k) {
-      case 0:
-        f=4.*(1.-t-x);
-        break;
-      case 1:
-        f=4.*z;
-        break;
-      }
-      break;
-    case 1:
-      f=4.*y;
-      break;
-    }
-    break;
-  case 2:
-    f=(-1.+4.*x);
-    break;
-  }
-  return f;
-}
-
-double tet2::dtet_2dy(const double& x, const double& y, const double &z, const int& i,const int& j, const int & k) const {
-  double f=0.;
-  double t=x+y+z;
-  switch (i) {
-  case 0:
-    switch (j) {
-    case 0:
-      switch (k) {
-      case 0:
-        f=(-3.+4.*t);
-        break;
-      case 1:
-        f=-4.*z;
-        break;
-      case 2:
-        f=0;
-        break;
-      }
-      break;
-    case 1:
-      switch (k) {
-      case 0:
-        f=4.*(1.-t-y);
-        break;
-      case 1:
-        f=4.*z;
-        break;
-      }
-      break;
-    case 2:
-      f=(-1.+4.*y);
-      break;
-    }
-    break;
-  case 1:
-    switch (j) {
-    case 0:
-      switch (k) {
-      case 0:
-        f=-4.*x;
-        break;
-      case 1:
-        f=0;
-        break;
-      }
-      break;
-    case 1:
-      f=4.*x;
-      break;
-    }
-    break;
-  case 2:
-    f=0.;
-    break;
-  }
-  return f;
-}
-
-double tet2::dtet_2dz(const double& x, const double& y, const double &z, const int& i,const int& j, const int & k) const {
-  double f=0.;
-  double t=x+y+z;
-  switch (i) {
-  case 0:
-    switch (j) {
-    case 0:
-      switch (k) {
-      case 0:
-        f=(-3.+4.*t);
-        break;
-      case 1:
-        f=4.*(1.-t-z);
-        break;
-      case 2:
-        f=(-1.+4.*z);
-        break;
-      }
-      break;
-    case 1:
-      switch (k) {
-      case 0:
-        f=-4.*y;
-        break;
-      case 1:
-        f=4.*y;
-        break;
-      }
-      break;
-    case 2:
-      f=0.;
-      break;
-    }
-    break;
-  case 1:
-    switch (j) {
-    case 0:
-      switch (k) {
-      case 0:
-        f=-4.*x;
-        break;
-      case 1:
-        f=4.*x;
-        break;
-      }
-      break;
-    case 1:
-      f=0.;
-      break;
-    }
-    break;
-  case 2:
-    f=0.;
-    break;
-  }
-  return f;
+double  tet2::eval_dphidy(const int *I,const double* X) const {
+  const double x=X[0];   const double y=X[1];   const double z=X[2];
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  
+  double t=1.-(x+y+z);
+  
+    return
+    !i 	  *( !j     *( !k*(-4.*t+1.) + !(k-1)*(-4.)*z )+
+	     !(j-1) *( !k*4.*(t-y)   + !(k-1)*4.*z    )+
+	     !(j-2) *( !k*(-1.+4.*y) 		      ) )+
+    !(i-1)*( !j     *( !k*(-4.)*x		      )+
+	     !(j-1) *( !k*4.*x			      ) );
+  
+  
+//   double f=0.;
+//   double t=x+y+z;
+//   switch (i) {
+//   case 0:
+//     switch (j) {
+//     case 0:
+//       switch (k) {
+//       case 0:
+//         f=(-3.+4.*t);
+//         break;
+//       case 1:
+//         f=-4.*z;
+//         break;
+//       case 2:
+//         f=0;
+//         break;
+//       }
+//       break;
+//     case 1:
+//       switch (k) {
+//       case 0:
+//         f=4.*(1.-t-y);
+//         break;
+//       case 1:
+//         f=4.*z;
+//         break;
+//       }
+//       break;
+//     case 2:
+//       f=(-1.+4.*y);
+//       break;
+//     }
+//     break;
+//   case 1:
+//     switch (j) {
+//     case 0:
+//       switch (k) {
+//       case 0:
+//         f=-4.*x;
+//         break;
+//       case 1:
+//         f=0;
+//         break;
+//       }
+//       break;
+//     case 1:
+//       f=4.*x;
+//       break;
+//     }
+//     break;
+//   case 2:
+//     f=0.;
+//     break;
+//   }
+//   return f;
 }
 
 
+double  tet2::eval_dphidz(const int *I,const double* X) const {
+  const double x=X[0];   const double y=X[1];   const double z=X[2];
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  
+  double t=1.-(x+y+z);
+  
+  return
+    !i 	  *( !j     *( !k*(-4.*t+1.) + !(k-1)*4.*(t-z) + !(k-2)*(-1+4.*z) )+
+	     !(j-1) *( !k*(-4.)*y    + !(k-1)*4.*y			     ) )+
+    !(i-1)*( !j     *( !k*(-4.)*x    + !(k-1)*4.*x			     ) );
+  
+  
+//   double f=0.;
+//   double t=x+y+z;
+//   switch (i) {
+//   case 0:
+//     switch (j) {
+//     case 0:
+//       switch (k) {
+//       case 0:
+//         f=(-3.+4.*t);
+//         break;
+//       case 1:
+//         f=4.*(1.-t-z);
+//         break;
+//       case 2:
+//         f=(-1.+4.*z);
+//         break;
+//       }
+//       break;
+//     case 1:
+//       switch (k) {
+//       case 0:
+//         f=-4.*y;
+//         break;
+//       case 1:
+//         f=4.*y;
+//         break;
+//       }
+//       break;
+//     case 2:
+//       f=0.;
+//       break;
+//     }
+//     break;
+//   case 1:
+//     switch (j) {
+//     case 0:
+//       switch (k) {
+//       case 0:
+//         f=-4.*x;
+//         break;
+//       case 1:
+//         f=4.*x;
+//         break;
+//       }
+//       break;
+//     case 1:
+//       f=0.;
+//       break;
+//     }
+//     break;
+//   case 2:
+//     f=0.;
+//     break;
+//   }
+//   return f;
+}
+
+double  tet2::eval_d2phidx2(const int *I,const double* X) const {
+  
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  return
+    !i 	  *( !j *( !k*(4.)   ) )+
+    !(i-1)*( !j *( !k*(-8.)  ) )+
+    !(i-2)*( !j *( !k*(4.)   ) );   
+}
+
+double  tet2::eval_d2phidy2(const int *I,const double* X) const {
+  
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  return
+    !i 	  *( !j     *( !k*(4.)  )+
+	     !(j-1) *( !k*(-8.) )+
+	     !(j-2) *( !k*(4.)  ) );
+}
+
+
+double  tet2::eval_d2phidz2(const int *I,const double* X) const {
+  
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  return
+    !i *( !j *( !k*(4.) + !(k-1)*(-8.) + !(k-2)*(4.) ));
+}
+
+double  tet2::eval_d2phidxdy(const int *I,const double* X) const {
+  
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  return
+    !i 	  *( !j     *( !k*(4.)  )+
+	     !(j-1) *( !k*(-4.) ) )+
+    !(i-1)*( !j     *( !k*(-4.) )+
+	     !(j-1) *( !k*(4.)	) );
+    
+}
+
+double  tet2::eval_d2phidydz(const int *I,const double* X) const {
+ 
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  return
+    !i 	  *( !j     *( !k*(4.)   + !(k-1)*(-4.) )+
+	     !(j-1) *( !k*(-4.)  + !(k-1)*(4.)	) );
+}
+
+double  tet2::eval_d2phidzdx(const int *I,const double* X) const {
+ 
+  const int i=I[0];      const int j=I[1];      const int k=I[2];
+  return
+    !i 	  *( !j     *( !k*(4.)  + !(k-1)*(-4.) ) )+
+    !(i-1)*( !j     *( !k*(-4.) + !(k-1)*4.    ) );
+  
+}
 
 } //end namespace femus
 
