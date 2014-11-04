17596067cb9a46848ecef4360ac94580f2ba52b0
eugenio aulisa <eugenio.aulisa@ttu.edu>
2014-11-03 19:52:17 -0600

    basis class finished

--- src/fe/Hexaedron.cpp
+++ src/fe/Hexaedron.cpp
@@ -1,17 +1,17 @@
 /*=========================================================================
 
- Program: FEMUS
- Module: Hexaedron
- Authors: Eugenio Aulisa
+  Program: FEMUS
+  Module: Hexaedron
+  Authors: Eugenio Aulisa
  
- Copyright (c) FEMTTU
- All rights reserved. 
+  Copyright (c) FEMTTU
+  All rights reserved. 
 
- This software is distributed WITHOUT ANY WARRANTY; without even
- the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- PURPOSE.  See the above copyright notice for more information.
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
 
-=========================================================================*/
+  =========================================================================*/
 
 //----------------------------------------------------------------------------
 // includes :
@@ -23,224 +23,188 @@
 
 namespace femus {
 
+  double hex0::eval_phi(const int *I,const double* x) const {
+    return 1;
+  }
 
+  //************************************************************
+  double hexpwl::eval_phi(const int *I,const double* x) const {
+    return I[0]+ x[0]*I[1] + x[1]*I[2] + x[2]*(1.-I[0])*(1.-I[1])*(1.-I[2]);
+  }
 
+  double hexpwl::eval_dphidx(const int *I,const double* x) const {
+    return I[1];
+  }
 
-double hex0::eval_phi(const int *I,const double* x) const {
-  return 1;
-}
+  double hexpwl::eval_dphidy(const int *I,const double* x) const {
+    return I[2];
+  }
 
-//************************************************************
-double hexpwl::eval_phi(const int *I,const double* x) const {
-  return I[0]+ x[0]*I[1] + x[1]*I[2] + x[2]*(1.-I[0])*(1.-I[1])*(1.-I[2]);
-}
+  double hexpwl::eval_dphidz(const int *I,const double* x) const {
+    return (1.-I[0])*(1.-I[1])*(1.-I[2]);
+  }
 
-double hexpwl::eval_dphidx(const int *I,const double* x) const {
-  return I[1];
-}
+  // ************************************************************
 
-double hexpwl::eval_dphidy(const int *I,const double* x) const {
-  return I[2];
-}
+  double hex1::eval_phi(const int *I,const double* x) const {
+    return lag1(x[0],I[0])*lag1(x[1],I[1])*lag1(x[2],I[2]);
+  }
 
-double hexpwl::eval_dphidz(const int *I,const double* x) const {
-  return (1.-I[0])*(1.-I[1])*(1.-I[2]);
-}
-//************************************************************
+  double hex1::eval_dphidx(const int *I,const double* x) const {
+    return dlag1(x[0],I[0])*lag1(x[1],I[1])*lag1(x[2],I[2]);
+  }
 
-//************************************************************
+  double hex1::eval_dphidy(const int *I,const double* x) const {
+    return lag1(x[0],I[0])*dlag1(x[1],I[1])*lag1(x[2],I[2]);
+  }
 
-double hex1::eval_phi(const int *I,const double* x) const {
-  return lag1(x[0],I[0])*lag1(x[1],I[1])*lag1(x[2],I[2]);
-}
+  double hex1::eval_dphidz(const int *I,const double* x) const {
+    return lag1(x[0],I[0])*lag1(x[1],I[1])*dlag1(x[2],I[2]);
+  }
 
-double hex1::eval_dphidx(const int *I,const double* x) const {
-  return dlag1(x[0],I[0])*lag1(x[1],I[1])*lag1(x[2],I[2]);
-}
+  double hex1::eval_d2phidxdy(const int *I,const double* x) const {
+    return dlag1(x[0],I[0])*dlag1(x[1],I[1])*lag1(x[2],I[2]);
+  }
 
-double hex1::eval_dphidy(const int *I,const double* x) const {
-  return lag1(x[0],I[0])*dlag1(x[1],I[1])*lag1(x[2],I[2]);
-}
+  double hex1::eval_d2phidydz(const int *I,const double* x) const {
+    return lag1(x[0],I[0])*dlag1(x[1],I[1])*dlag1(x[2],I[2]);
+  }
 
-double hex1::eval_dphidz(const int *I,const double* x) const {
-  return lag1(x[0],I[0])*lag1(x[1],I[1])*dlag1(x[2],I[2]);
-}
+  double hex1::eval_d2phidzdx(const int *I,const double* x) const {
+    return dlag1(x[0],I[0])*lag1(x[1],I[1])*dlag1(x[2],I[2]);
+  }
 
-double hex1::eval_d2phidxdy(const int *I,const double* x) const {
-  return dlag1(x[0],I[0])*dlag1(x[1],I[1])*lag1(x[2],I[2]);
-}
+  // ************************************************************
 
-double hex1::eval_d2phidydz(const int *I,const double* x) const {
-  return lag1(x[0],I[0])*dlag1(x[1],I[1])*dlag1(x[2],I[2]);
-}
+  double hex2::eval_phi(const int *I,const double* x) const {
+    return lag2(x[0],I[0])*lag2(x[1],I[1])*lag2(x[2],I[2]);
+  }
 
-double hex1::eval_d2phidzdx(const int *I,const double* x) const {
-  return dlag1(x[0],I[0])*lag1(x[1],I[1])*dlag1(x[2],I[2]);
-}
+  double hex2::eval_dphidx(const int *I,const double* x) const {
+    return dlag2(x[0],I[0])*lag2(x[1],I[1])*lag2(x[2],I[2]);
+  }
 
+  double hex2::eval_dphidy(const int *I,const double* x) const {
+    return lag2(x[0],I[0])*dlag2(x[1],I[1])*lag2(x[2],I[2]);
+  }
 
-double hex1::lag1(const double& x, const int& i) const {
-  return (!i)*0.5*(1.-x)+!(i-2)*0.5*(1.+x);
-}
-double hex1::dlag1(const double& x, const int& i) const {
-  return (!i)*(-0.5)+!(i-2)*0.5;
-}
+  double hex2::eval_dphidz(const int *I,const double* x) const {
+    return lag2(x[0],I[0])*lag2(x[1],I[1])*dlag2(x[2],I[2]);
+  }
 
+  double hex2::eval_d2phidx2(const int *I,const double* x) const {
+    return d2lag2(x[0],I[0])*lag2(x[1],I[1])*lag2(x[2],I[2]);
+  }
 
-//************************************************************
+  double hex2::eval_d2phidy2(const int *I,const double* x) const {
+    return lag2(x[0],I[0])*d2lag2(x[1],I[1])*lag2(x[2],I[2]);
+  }
 
-double hex2::eval_phi(const int *I,const double* x) const {
-  return lag2(x[0],I[0])*lag2(x[1],I[1])*lag2(x[2],I[2]);
-}
+  double hex2::eval_d2phidz2(const int *I,const double* x) const {
+    return lag2(x[0],I[0])*lag2(x[1],I[1])*d2lag2(x[2],I[2]);
+  }
 
-double hex2::eval_dphidx(const int *I,const double* x) const {
-  return dlag2(x[0],I[0])*lag2(x[1],I[1])*lag2(x[2],I[2]);
-}
+  double hex2::eval_d2phidxdy(const int *I,const double* x) const {
+    return dlag2(x[0],I[0])*dlag2(x[1],I[1])*lag2(x[2],I[2]);
+  }
 
-double hex2::eval_dphidy(const int *I,const double* x) const {
-  return lag2(x[0],I[0])*dlag2(x[1],I[1])*lag2(x[2],I[2]);
-}
+  double hex2::eval_d2phidydz(const int *I,const double* x) const {
+    return lag2(x[0],I[0])*dlag2(x[1],I[1])*dlag2(x[2],I[2]);
+  }
 
-double hex2::eval_dphidz(const int *I,const double* x) const {
-  return lag2(x[0],I[0])*lag2(x[1],I[1])*dlag2(x[2],I[2]);
-}
+  double hex2::eval_d2phidzdx(const int *I,const double* x) const {
+    return dlag2(x[0],I[0])*lag2(x[1],I[1])*dlag2(x[2],I[2]);
+  }
 
-double hex2::eval_d2phidx2(const int *I,const double* x) const {
-  return d2lag2(x[0],I[0])*lag2(x[1],I[1])*lag2(x[2],I[2]);
-}
+  // ************************************************************
 
-double hex2::eval_d2phidy2(const int *I,const double* x) const {
-  return lag2(x[0],I[0])*d2lag2(x[1],I[1])*lag2(x[2],I[2]);
-}
+  double hexth::eval_phi(const int *I,const double* x) const {
+    const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)? 
+      th2(x[0],I[0])*th2(x[1],I[1])*th2(x[2],I[2])
+      : 
+      (-2.+ix*x[0]+jx*x[1]+kx*x[2])*th2(x[0],I[0])*th2(x[1],I[1])*th2(x[2],I[2]);
+  }
 
-double hex2::eval_d2phidz2(const int *I,const double* x) const {
-  return lag2(x[0],I[0])*lag2(x[1],I[1])*d2lag2(x[2],I[2]);
-}
+  double hexth::eval_dphidx(const int *I,const double* x) const {
+    const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)?
+      dth2(x[0],I[0])*th2(x[1],I[1])*th2(x[2],I[2]) 
+      :
+      th2(x[1],I[1])*th2(x[2],I[2])*(ix*th2(x[0],I[0]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[0],I[0]));
+  }
 
-double hex2::eval_d2phidxdy(const int *I,const double* x) const {
-  return dlag2(x[0],I[0])*dlag2(x[1],I[1])*lag2(x[2],I[2]);
-}
+  double hexth::eval_dphidy(const int *I,const double* x) const {
+    const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)?
+      th2(x[0],I[0])*dth2(x[1],I[1])*th2(x[2],I[2]) 
+      :
+      th2(x[0],I[0])*th2(x[2],I[2])*(jx*th2(x[1],I[1]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[1],I[1]));
+  }
 
-double hex2::eval_d2phidydz(const int *I,const double* x) const {
-  return lag2(x[0],I[0])*dlag2(x[1],I[1])*dlag2(x[2],I[2]);
-}
+  double hexth::eval_dphidz(const int *I,const double* x) const {
+    const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)?
+      th2(x[0],I[0])*th2(x[1],I[1])*dth2(x[2],I[2]) 
+      :
+      th2(x[0],I[0])*th2(x[1],I[1])*(kx*th2(x[2],I[2]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[2],I[2]));
+  }
 
-double hex2::eval_d2phidzdx(const int *I,const double* x) const {
-  return dlag2(x[0],I[0])*lag2(x[1],I[1])*dlag2(x[2],I[2]);
-}
+  double hexth::eval_d2phidx2(const int *I,const double* x) const {
+    const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)?
+      d2th2(x[0],I[0])*th2(x[1],I[1])*th2(x[2],I[2]) 
+      :
+      th2(x[1],I[1])*th2(x[2],I[2])*( 2.*ix*dth2(x[0],I[0]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*d2th2(x[0],I[0]) );
+  }
 
-double hex2::lag2(const double& x, const int& i) const {
-  return !i*0.5*x*(x-1.) + !(i-1)*(1.-x)*(1.+x) + !(i-2)*0.5*x*(1.+x);
-}
+  double hexth::eval_d2phidy2(const int *I,const double* x) const {
+    const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)?
+      th2(x[0],I[0])*d2th2(x[1],I[1])*th2(x[2],I[2]) 
+      :
+      th2(x[2],I[2])*th2(x[0],I[0])*( 2.*jx*dth2(x[1],I[1]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*d2th2(x[1],I[1]) );
+  }
 
-double hex2::dlag2(const double& x, const int& i) const {
-  return !i*(x-0.5) + !(i-1)*(-2.*x) + !(i-2)*(x+0.5);
-}
+  double hexth::eval_d2phidz2(const int *I,const double* x) const {
+    const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)?
+      th2(x[0],I[0])*th2(x[1],I[1])*d2th2(x[2],I[2]) 
+      :
+      th2(x[0],I[0])*th2(x[1],I[1])*( 2.*kx*dth2(x[2],I[2]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*d2th2(x[2],I[2]) );
+  }
 
-double hex2::d2lag2(const double& x, const int& i) const {
-  return !i + !(i-1)*(-2.) + !(i-2);
-}
-//************************************************************
-
-double hexth::eval_phi(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)? 
-	  th2(x[0],I[0])*th2(x[1],I[1])*th2(x[2],I[2])
-	  : 
-	  (-2.+ix*x[0]+jx*x[1]+kx*x[2])*th2(x[0],I[0])*th2(x[1],I[1])*th2(x[2],I[2]);
-}
-
-double hexth::eval_dphidx(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)?
-	  dth2(x[0],I[0])*th2(x[1],I[1])*th2(x[2],I[2]) 
-	  :
-          th2(x[1],I[1])*th2(x[2],I[2])*(ix*th2(x[0],I[0]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[0],I[0]));
-}
-
-double hexth::eval_dphidy(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)?
-          th2(x[0],I[0])*dth2(x[1],I[1])*th2(x[2],I[2]) 
-	  :
-          th2(x[0],I[0])*th2(x[2],I[2])*(jx*th2(x[1],I[1]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[1],I[1]));
-}
-
-double hexth::eval_dphidz(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)?
-          th2(x[0],I[0])*th2(x[1],I[1])*dth2(x[2],I[2]) 
-	  :
-          th2(x[0],I[0])*th2(x[1],I[1])*(kx*th2(x[2],I[2]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[2],I[2]));
-}
-
-double hexth::eval_d2phidx2(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)?
-          d2th2(x[0],I[0])*th2(x[1],I[1])*th2(x[2],I[2]) 
-	  :
-          th2(x[1],I[1])*th2(x[2],I[2])*( 2.*ix*dth2(x[0],I[0]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*d2th2(x[0],I[0]) );
-}
-
-double hexth::eval_d2phidy2(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)?
-          th2(x[0],I[0])*d2th2(x[1],I[1])*th2(x[2],I[2]) 
-	  :
-          th2(x[2],I[2])*th2(x[0],I[0])*( 2.*jx*dth2(x[1],I[1]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*d2th2(x[1],I[1]) );
-}
-
-double hexth::eval_d2phidz2(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)?
-          th2(x[0],I[0])*th2(x[1],I[1])*d2th2(x[2],I[2]) 
-	  :
-          th2(x[0],I[0])*th2(x[1],I[1])*( 2.*kx*dth2(x[2],I[2]) + (-2.+ix*x[0]+jx*x[1]+kx*x[2])*d2th2(x[2],I[2]) );
-}
-
-double hexth::eval_d2phidxdy(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)?
-          dth2(x[0],I[0])*dth2(x[1],I[1])*th2(x[2],I[2]) 
-	  :
-          th2(x[2],I[2])*( ix*th2(x[0],I[0])*dth2(x[1],I[1]) + 
-			   jx*th2(x[1],I[1])*dth2(x[0],I[0]) +
-			   (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[0],I[0])*dth2(x[1],I[1]));
+  double hexth::eval_d2phidxdy(const int *I,const double* x) const {
+    const double ix=I[0]-1., jx=I[1]-1., kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)?
+      dth2(x[0],I[0])*dth2(x[1],I[1])*th2(x[2],I[2]) 
+      :
+      th2(x[2],I[2])*( ix*th2(x[0],I[0])*dth2(x[1],I[1]) + 
+		       jx*th2(x[1],I[1])*dth2(x[0],I[0]) +
+		       (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[0],I[0])*dth2(x[1],I[1]));
 	      
-}
+  }
 
-double hexth::eval_d2phidydz(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)?
-          th2(x[0],I[0])*dth2(x[1],I[1])*dth2(x[2],I[2]) 
-	  :
-	  th2(x[0],I[0])*( jx*th2(x[1],I[1])*dth2(x[2],I[2]) + 
-			   kx*th2(x[2],I[2])*dth2(x[1],I[1]) +
-			   (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[1],I[1])*dth2(x[2],I[2]));
+  double hexth::eval_d2phidydz(const int *I,const double* x) const {
+    const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)?
+      th2(x[0],I[0])*dth2(x[1],I[1])*dth2(x[2],I[2]) 
+      :
+      th2(x[0],I[0])*( jx*th2(x[1],I[1])*dth2(x[2],I[2]) + 
+		       kx*th2(x[2],I[2])*dth2(x[1],I[1]) +
+		       (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[1],I[1])*dth2(x[2],I[2]));
 	    
-}
+  }
 
-double hexth::eval_d2phidzdx(const int *I,const double* x) const {
-  const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
-  return (fabs(ix*jx*kx)==0)?
-          dth2(x[0],I[0])*th2(x[1],I[1])*dth2(x[2],I[2]) 
-	  :
-          th2(x[1],I[1])*( kx*th2(x[2],I[2])*dth2(x[0],I[0]) + 
-			   ix*th2(x[0],I[0])*dth2(x[2],I[2]) +
-			   (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[2],I[2])*dth2(x[0],I[0]));
-}
-
-double hexth::th2(const double& x, const int& i) const {
-  return !i*(0.5)*(1.-x) + !(i-1)*(1.-x)*(1.+x) + !(i-2)*(0.5)*(1.+x);
-}
-
-double hexth::dth2(const double& x, const int& i) const {
-  return (!i)*(-0.5) + !(i-1)*(-2.*x) + !(i-2)*(0.5);
-}
-
-double hexth::d2th2(const double& x, const int& i) const {
-  return  !(i-1)*(-2.);
-}
+  double hexth::eval_d2phidzdx(const int *I,const double* x) const {
+    const double ix=I[0]-1.,jx=I[1]-1.,kx=I[2]-1.;
+    return (fabs(ix*jx*kx)==0)?
+      dth2(x[0],I[0])*th2(x[1],I[1])*dth2(x[2],I[2]) 
+      :
+      th2(x[1],I[1])*( kx*th2(x[2],I[2])*dth2(x[0],I[0]) + 
+		       ix*th2(x[0],I[0])*dth2(x[2],I[2]) +
+		       (-2.+ix*x[0]+jx*x[1]+kx*x[2])*dth2(x[2],I[2])*dth2(x[0],I[0]));
+  }
 
 } //end namespace femus
 
--- src/fe/Triangle.cpp
+++ src/fe/Triangle.cpp
@@ -1,17 +1,17 @@
 /*=========================================================================
 
- Program: FEMUS
- Module: Triangle
- Authors: Eugenio Aulisa
+  Program: FEMUS
+  Module: Triangle
+  Authors: Eugenio Aulisa
  
- Copyright (c) FEMTTU
- All rights reserved. 
+  Copyright (c) FEMTTU
+  All rights reserved. 
 
- This software is distributed WITHOUT ANY WARRANTY; without even
- the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- PURPOSE.  See the above copyright notice for more information.
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
 
-=========================================================================*/
+  =========================================================================*/
 
 //----------------------------------------------------------------------------
 // includes :
@@ -21,188 +21,49 @@
 
 namespace femus {
 
+  double tri0::eval_phi(const int *I,const double* x) const {
+    return 1.;
+  }
 
-double tri0::eval_phi(const int *I,const double* x) const {
-  return 1.;
-}
+  //************************************************************
 
-//************************************************************
+  double tri1::eval_phi(const int *I,const double* x) const {
+    return triangle1(x[0],x[1],I[0],I[1]);
+  }
 
-double tri1::eval_phi(const int *I,const double* x) const {
-  return tri1a(x[0],x[1],I[0],I[1]);
-}
+  double tri1::eval_dphidx(const int *I,const double* x) const {
+    return dtriangle1dx(x[0],x[1],I[0],I[1]);
+  }
 
-double tri1::eval_dphidx(const int *I,const double* x) const {
-  return dtri1dx(x[0],x[1],I[0],I[1]);
-}
+  double tri1::eval_dphidy(const int *I,const double* x) const {
+    return dtriangle1dy(x[0],x[1],I[0],I[1]);
+  }
 
-double tri1::eval_dphidy(const int *I,const double* x) const {
-  return dtri1dy(x[0],x[1],I[0],I[1]);
-}
+  //************************************************************
 
-double tri1::tri1a(const double& x, const double& y, const int& i,const int& j) const {
-  return (!i*!j)*(1.-x-y)+ !(i-2)*x + !(j-2)*y;
-}
+  double tri2::eval_phi(const int *I,const double* x) const {
+    return triangle2(x[0],x[1],I[0],I[1]);
+  }
 
-double tri1::dtri1dx(const double& x, const double& y, const int& i,const int& j) const {
-  return -(!i*!j) + !(i-2);
-}
+  double tri2::eval_dphidx(const int *I,const double* x) const {
+    return dtriangle2dx(x[0],x[1],I[0],I[1]);
+  }
 
-double tri1::dtri1dy(const double& x, const double& y, const int& i,const int& j) const {
-  return -(!i*!j) + !(j-2);
-}
+  double tri2::eval_dphidy(const int *I,const double* x) const {
+    return dtriangle2dy(x[0],x[1],I[0],I[1]);
+  }
 
-//************************************************************
+  double tri2::eval_d2phidx2(const int *I,const double* x) const {
+    return d2triangle2dx2(x[0],x[1],I[0],I[1]);
+  }
 
-double tri2::eval_phi(const int *I,const double* x) const {
-  return tri2a(x[0],x[1],I[0],I[1]);
-}
+  double tri2::eval_d2phidy2(const int *I,const double* x) const {
+    return d2triangle2dy2(x[0],x[1],I[0],I[1]);
+  }
 
-double tri2::eval_dphidx(const int *I,const double* x) const {
-  return dtri2dx(x[0],x[1],I[0],I[1]);
-}
-
-double tri2::eval_dphidy(const int *I,const double* x) const {
-  return dtri2dy(x[0],x[1],I[0],I[1]);
-}
-
-double tri2::eval_d2phidx2(const int *I,const double* x) const {
-  return d2tri2dx2(x[0],x[1],I[0],I[1]);
-}
-
-double tri2::eval_d2phidy2(const int *I,const double* x) const {
-  return d2tri2dy2(x[0],x[1],I[0],I[1]);
-}
-
-double tri2::eval_d2phidxdy(const int *I,const double* x) const {
-  return d2tri2dxdy(x[0],x[1],I[0],I[1]);
-}
-
-double tri2::tri2a(const double& x, const double& y, const int& i,const int& j) const {
+  double tri2::eval_d2phidxdy(const int *I,const double* x) const {
+    return d2triangle2dxdy(x[0],x[1],I[0],I[1]);
+  }
   
-  return !i     * (!j* (1.-x-y)*(1.-2.*x-2.*y) + !(j-1)* 4.*y*(1.-x-y) + !(j-2)*(-y+2.*y*y)) +
-	 !(i-1) * (!j* 4.*x*(1.-x-y) + !(j-1) * 4.*x*y) +
-	 !(i-2) * (!j*(-x+2.*x*x));   
-//   double f=0.;
-//   switch (i) {
-//   case(0):
-//     switch (j) {
-//     case(0):
-//       f=(1.-x-y)*(1.-2.*x-2.*y);
-//       break;
-//     case(1):
-//       f=4.*y*(1.-x-y);
-//       break;
-//     case(2):
-//       f=(-y+2.*y*y);
-//       break;
-//     }
-//     break;
-//   case(1):
-//     switch (j) {
-//     case(0):
-//       f=4.*x*(1.-x-y);
-//       break;
-//     case(1):
-//       f=4.*x*y;
-//       break;
-//     }
-//     break;
-//   case(2):
-//     f=(-x+2.*x*x);
-//     break;
-//   }
-//   return f;
-}
-
-double tri2::dtri2dx(const double& x, const double& y, const int& i,const int& j) const {
-  
-  return !i     * (!j* (-3.+4.*x+4.*y) + !(j-1)*y*(-4.) ) +
-	 !(i-1) * (!j* 4.*(1.-2.*x-y)  + !(j-1)*y*(4.)) +
-	 !(i-2) * (!j*(-1 + 4.*x));
-    
-//   double f=0.;
-//   switch (i) {
-//   case(0):
-//     switch (j) {
-//     case(0):
-//       f=(-3.+4.*x+4.*y);
-//       break;
-//     case(1):
-//       f=-4.*y;
-//       break;
-//     case(2):
-//       f=0;
-//       break;
-//     }
-//     break;
-//   case(1):
-//     switch (j) {
-//     case(0):
-//       f=4.*(1-2.*x-y);
-//       break;
-//     case(1):
-//       f=4.*y;
-//       break;
-//     }
-//     break;
-//   case(2):
-//     f=-1.+4.*x;
-//     break;
-//   }
-//   return f;
-}
-
-
-double tri2::dtri2dy(const double& x, const double& y, const int& i,const int& j) const {
-  
-  return !j     * (!i* (-3.+4.*y+4.*x) + !(i-1)*x*(-4.) ) +
-	 !(j-1) * (!i* 4.*(1.-2.*y-x)  + !(i-1)*x*(4.)) +
-	 !(j-2) * (!i*(-1 + 4.*y));	 
-	  
-//   double f=0.;
-//   switch (i) {
-//   case(0):
-//     switch (j) {
-//     case(0):
-//       f=(-3.+4.*x+4.*y);
-//       break;
-//     case(1):
-//       f=4.*(1-x-2.*y);
-//       break;
-//     case(2):
-//       f=-1.+4.*y;
-//       break;
-//     }
-//     break;
-//   case(1):
-//     switch (j) {
-//     case(0):
-//       f=-4.*x;
-//       break;
-//     case(1):
-//       f=4.*x;
-//       break;
-//     }
-//     break;
-//   case(2):
-//     f=0.;
-//     break;
-//   }
-//   return f;
-}
-
-double tri2::d2tri2dx2(const double& x, const double& y, const int& i,const int& j) const {
-  return !j*( (!i)*4. +!(i-1)*(-8.) + !(i-2)*4. );
-}
-
-double tri2::d2tri2dy2(const double& x, const double& y, const int& i,const int& j) const {
-  return !i*( (!j)*4. +!(j-1)*(-8.) + !(j-2)*4. );
-}
-
-double tri2::d2tri2dxdy(const double& x, const double& y, const int& i,const int& j) const {
-  return (!i*!j + !(i-1)*!(j-1))*4. + (!(i-1)*!j + !i*!(j-1))*(-4.);
-}
-
 } //end namespace femus
 
