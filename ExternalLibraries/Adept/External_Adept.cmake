246d340a0ac9333cbfb4a1f670c3eb826789d705
simone.bna <simone.bna@unibo.it>
2014-11-06 23:36:39 +0100

    first riorganization of the main cmakelist file

--- ExternalLibraries/Adept/External_Adept.cmake
+++ ExternalLibraries/Adept/External_Adept.cmake
@@ -5,7 +5,7 @@
 SET(ADEPT_VERSION 1.0)
 SET(ADEPT_SOURCE_DIR ${PROJECT_SOURCE_DIR}/ExternalLibraries/Adept/adept-${ADEPT_VERSION})
 
-#look if there is a package for VTK
+#look if there is a package for adept
 IF (EXISTS "${ADEPT_SOURCE_DIR}")   
   MESSAGE(STATUS "ADEPT: Found ADEPT packages")
 ENDIF (EXISTS "${ADEPT_SOURCE_DIR}")
