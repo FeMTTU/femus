dc1d20ba1bc5c5a915e84c0359ccf2f4bc204eaa
Giorgio Bornia <giorgio.bornia@ttu.edu>
2015-01-16 23:38:21 -0600

    Working on System and EqnBase

--- src/equations/EqnBase.hpp
+++ src/equations/EqnBase.hpp
@@ -41,29 +41,31 @@ public:
 //======== Vectors =============== (procs,levels) ==
 //=======================================================================
 
-  std::vector<NumericVector *> _b;   ///< rhs b
-  std::vector<NumericVector *> _x;   ///< solution x
-  std::vector<NumericVector *> _res; ///< residual
+  std::vector<NumericVector *> _b;   //// LinearEquation (each level)
+  std::vector<NumericVector *> _x;   //// LinearEquation (each level)
+  std::vector<NumericVector *> _res; //// LinearEquation (each level)
 
-  std::vector<NumericVector *> _x_old;
+  std::vector<NumericVector *> _x_old; //// LinearEquation (each level)
+  
   std::vector<NumericVector *> _x_oold;
   std::vector<NumericVector *> _x_tmp;
+        double MGTimeStep(const uint iter);                    ///< MG time step solver (backward Euler)
 
           void  initVectors();                                      ///initialize vectors
-          void PrintVector(std::string namefile);   ///prints on a "Quadratic-Linearized" Mesh //TODO this should be PrintNumericVector of the equation 
-          void  ReadVector(std::string namefile);                       ///read from a "Quadratic-Linearized" Mesh 
+          void PrintVector(std::string namefile);   ///prints on a "Quadratic-Linearized" Mesh //TODO this should be PrintNumericVector of the equation //Writer//
+          void  ReadVector(std::string namefile);                       ///read from a "Quadratic-Linearized" Mesh                                      //Writer/Reader// 
 
 //=======================================================================
 //======= Quantities =========
 //=======================================================================
-      std::vector<Quantity*>          _QtyInternalVector;
+      std::vector<Quantity*>          _QtyInternalVector;  //System//
   
 //=======================================================================
   //====== Attributes of the equation ====
 //=======================================================================
-  const std::string _eqname;   ///< equation name
+  const std::string _eqname;   ///< equation name     //System//
   uint               _iproc;   ///< processor rank
-  const uint      _NoLevels;   ///< level number
+  const uint      _NoLevels;   ///< level number      //System//
 
 //=======================================================================
 // CONSTRUCTOR / DESTRUCTOR
@@ -71,16 +73,16 @@ public:
   EqnBase(std::vector<Quantity*> int_map_in,
 	  EquationsMap& equations_map,
           std::string eq_name_in="Base",
-	  std::string varname_in="u");
+	  std::string varname_in="u");   //System//
   
-  virtual ~EqnBase();
+  virtual ~EqnBase(); //System//
   
 //=======================================================================
 //==== DOF MAP of the equation ============ (procs,levels) ==============
 //=======================================================================
 //====== data =======
   int    **  _node_dof; ///< dof map
-  uint  *    _Dim;            ///< dimension //number of dofs per level
+  uint  *    _Dim;            //number of dofs per level
   uint **    _DofNumLevFE;     
   uint **    _DofOffLevFE;     
   uint ***   _DofLocLevProcFE;     
@@ -92,28 +94,27 @@ public:
 //====== functions =======
           void initNVars();
 	  void initVarNamesRefValues(std::string varname_in);
-          void ComputeMeshToDof();     ///  distributing dof function
+          void ComputeMeshToDof();
           void PrintMeshToDof() const;
 
 
 //=======================================================================
 //========= MULTIGRID FUNCTIONS (Vectors + A,R,P) ======== (procs,levels) 
 //=======================================================================
-    void ReadMGOps();                                           ///initialize A, R, P and read
-    void ReadMatrix(const std::string& name);  ///< Reading matrix A
-    void ReadProl(const std::string& name);    ///< Reading Prolongation
-    void ReadRest(const std::string& name);    ///< Reading Restriction
+    void ReadMGOps();
+    void ReadMatrix(const std::string& name);
+    void ReadProl(const std::string& name);
+    void ReadRest(const std::string& name);
     void ComputeMatrix();
     void ComputeProl();
     void ComputeRest();
     void PrintOneVarMatrixHDF5(std::string name, std::string groupname, uint** n_nodes_all, int count,int* Mat,int* len,int* len_off,int type1, int type2, int* FELevel ) const;
     void PrintOneVarMGOperatorHDF5(std::string filename, std::string groupname, uint* n_dofs_lev, int count,int* Rest,double* values,int* len,int* len_off, int FELevel, int FELevel2, int fe) const;
 
- virtual  void GenMatRhs(const uint Level) = 0;  //every equation must have one, explicitly!
-        double MGTimeStep(const uint iter);                    ///< MG time step solver (backward Euler)
-          void MGSolve(double Eps,int MaxIter, const uint Gamma=DEFAULT_MG_GAMMA, const uint Nc_pre=DEFAULT_NC_PRE,const uint Nc_coarse=DEFAULT_NC_COARSE,const uint Nc_post=DEFAULT_NC_POST);    ///< MultiGrid Solver
-        double MGStep(int Level,double Eps1,int MaxIter, const uint Gamma, const uint Nc_pre,const uint Nc_coarse,const uint Nc_post);    ///< MultiGrid Step
-          void MGCheck(int Level) const;                                           ///< Check Operators
+ virtual  void GenMatRhs(const uint Level) = 0;  //System//
+          void MGSolve(double Eps,int MaxIter, const uint Gamma=DEFAULT_MG_GAMMA, const uint Nc_pre=DEFAULT_NC_PRE,const uint Nc_coarse=DEFAULT_NC_COARSE,const uint Nc_post=DEFAULT_NC_POST);  //PetscLinearSolverM//
+        double MGStep(int Level,double Eps1,int MaxIter, const uint Gamma, const uint Nc_pre,const uint Nc_coarse,const uint Nc_post);                                                          //PetscLinearSolverM//
+          void MGCheck(int Level) const;
 
 
 //=======================================================================
@@ -147,28 +148,26 @@ public:
           void Bc_AddDofVec(NumericVector* myvec, NumericVector* myvec2 );
           void Bc_AddScaleDofVec(NumericVector* vec_in,NumericVector* vec_out,const double ScaleFac );
 
-//=======================================================================
-//======= MG: Linear Solvers for every Level ============
-//=======================================================================
-  LinearEquationSolver **_solver;     ///< linear system solver type (each level)
+//======= Linear Solvers for every Level ============
+  LinearEquationSolver **_solver;     ///(each level)
 
 protected:
   
 //=======================================================================
 // ====== data pointer ==========
 //=======================================================================
-  Files                     & _files;        ///<  file class pointer
-  Physics                   & _phys;         ///<  parameter class pointer
-  MeshTwo                   & _mesh;         ///<  mesh pointer
-  std::vector<FEElemBase*>  &  _AbstractFE;  ///<  FE
-  EquationsMap              & _eqnmap;       ///<  equation map  pointer
+  Files                     & _files;
+  Physics                   & _phys;           //In MultilevelProblem
+  MeshTwo                   & _mesh;
+  std::vector<FEElemBase*>  &  _AbstractFE;
+  EquationsMap              & _eqnmap;
 
 //=======================================================================
 //======== MG Ops ============ (procs,levels) ====
 //=======================================================================
-  std::vector<SparseMatrix  *> _A;    ///< Matrix A
-  std::vector<SparseMatrix *> _Rst; ///< Restrictor
-  std::vector<SparseMatrix *> _Prl; ///< Prolongation
+  std::vector<SparseMatrix  *> _A;  // LinearEquation (each level)
+  std::vector<SparseMatrix *> _Rst; // LinearEquation (each level)
+  std::vector<SparseMatrix *> _Prl; // LinearEquation (each level)
 
 };
 
