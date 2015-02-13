ab1d1e16f158119212d3f19ad81275677c123844
tparagod <thanuja.paragoda@ttu.edu>
2015-02-13 17:51:57 -0500

    init

--- applications/NumPdeSpring2015/Thanuja/main.cpp
+++ applications/NumPdeSpring2015/Thanuja/main.cpp
@@ -1,11 +1,12 @@
-// #include "MultiLevelProblem.hpp"
+ #include "MultiLevelProblem.hpp"
 // #include "TransientSystem.hpp"
 // #include "NumericVector.hpp"
 // #include "Fluid.hpp"
 // #include "Parameter.hpp"
 
 // #include "SparseMatrix.hpp"
-// #include "VTKWriter.hpp"
+#include "VTKWriter.hpp"
+#include "GMVWriter.hpp"
 // #include "NonLinearImplicitSystem.hpp"
 // #include "FElemTypeEnum.hpp"
 // #include "Files.hpp"
@@ -17,11 +18,62 @@
 #include "FemusInit.hpp"
 using namespace femus;
 
-int main(int argc,char **args) {
+double InitalValueU(const double &x, const double &y, const double &z){
+  return x+y;
+}
+
+double InitalValueP(const double &x, const double &y, const double &z){
+  return x;
+}
+
+double InitalValueT(const double &x, const double &y, const double &z){
+  return y;
+}
+
+int main(int argc, char **args) {
   
   /// Init Petsc-MPI communicator
-  FemusInit mpinit(argc,args,MPI_COMM_WORLD);
+  FemusInit mpinit(argc, args, MPI_COMM_WORLD);
  
-  return 0;
+  // read coarse level mesh and generate finers level meshes
+  MultiLevelMesh mlMsh;
+  double scalingFactor=1.; 
+  mlMsh.ReadCoarseMesh("./input/square.neu","seventh",scalingFactor); 
+  /* "seventh" is the order of accuracy that is used in the gauss integration scheme
+      probably in the furure it is not going to be an argument of this function   */
+  unsigned numberOfLevels=2;
+  unsigned numberOfSelectiveLevels=0;
+  mlMsh.RefineMesh(numberOfLevels , numberOfLevels + numberOfSelectiveLevels, NULL);
+  mlMsh.PrintInfo();
+  
+  // define and initialize variables
+  MultiLevelSolution mlSol(&mlMsh);
+  
+  mlSol.AddSolution("U",LAGRANGE, FIRST);
+  mlSol.AddSolution("V",LAGRANGE, SERENDIPITY);
+  mlSol.AddSolution("W",LAGRANGE, SECOND);
+  mlSol.AddSolution("P",DISCONTINOUS_POLYNOMIAL, ZERO);
+  mlSol.AddSolution("T",DISCONTINOUS_POLYNOMIAL, FIRST);
+  
+  mlSol.Initialize("All");  
+  mlSol.Initialize("U", InitalValueU);
+  mlSol.Initialize("P", InitalValueP);
+  mlSol.Initialize("T", InitalValueT); //note that this initialization is the same as piecewise constant element
+  
+  // print solutions
+  std::vector < std::string > variablesToBePrinted;
+  variablesToBePrinted.push_back("U");
+  variablesToBePrinted.push_back("P");
+  variablesToBePrinted.push_back("T");
+
+  VTKWriter vtkIO(mlSol);
+  vtkIO.write_system_solutions(DEFAULT_OUTPUTDIR, "biquadratic", variablesToBePrinted);
+
+  GMVWriter gmvIO(mlSol);
+  variablesToBePrinted.push_back("all");
+  gmvIO.SetDebugOutput(true);
+  gmvIO.write_system_solutions(DEFAULT_OUTPUTDIR, "biquadratic", variablesToBePrinted);
+   
+  return 1;
 }
 
