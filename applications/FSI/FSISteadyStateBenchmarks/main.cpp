04daef90932e72899334eaeb98f43409a1cdbc69
eugenio aulisa <eugenio.aulisa@ttu.edu>
2015-08-26 07:31:45 -0500

    opt. parameters in FSI

--- applications/FSI/FSISteadyStateBenchmarks/main.cpp
+++ applications/FSI/FSISteadyStateBenchmarks/main.cpp
@@ -273,19 +273,19 @@ int main(int argc,char **args) {
   // ******* set MG-Solver *******
   system.SetMgType(F_CYCLE);
   system.SetLinearConvergenceTolerance(1.e-10);
-  system.SetNonLinearConvergenceTolerance(1.e-10);
+  system.SetNonLinearConvergenceTolerance(1.e-9);
   if( simulation == 7 )
     system.SetNonLinearConvergenceTolerance(1.e-5);
 
   system.SetNumberPreSmoothingStep(15);
   system.SetNumberPostSmoothingStep(15);
-  
+
   if( simulation < 3 || simulation == 7 ) {
-    system.SetMaxNumberOfLinearIterations(3);
+    system.SetMaxNumberOfLinearIterations(2);
     system.SetMaxNumberOfNonLinearIterations(15);
   }
   else {
-    system.SetMaxNumberOfLinearIterations(8);
+    system.SetMaxNumberOfLinearIterations(2);
     system.SetMaxNumberOfNonLinearIterations(15);
   }
 
@@ -323,7 +323,7 @@ int main(int argc,char **args) {
     //system.SetElementBlockFluidAll();
   }
   else if(simulation == 7 ){
-    system.SetElementBlockNumber(3);
+    system.SetElementBlockNumber(2);
   }
 
   // ******* For Gmres Preconditioner only *******
