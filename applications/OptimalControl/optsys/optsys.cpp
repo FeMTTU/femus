37fd57ad42d84a97565c2eadd4db28c7bc27e9ee
Saikanth Ratnavale <saikanth.ratnavale@ttu.edu>
2015-11-02 10:32:29 -0600

    Ready to implement the equations

--- applications/OptimalControl/optsys/optsys.cpp
+++ applications/OptimalControl/optsys/optsys.cpp
@@ -1,20 +1,8 @@
-/** tutorial/Ex1
- * This example shows how to:
- * initialize a femus application;
- * define the multilevel-mesh object mlMsh;
- * read from the file ./input/square.neu the coarse-level mesh and associate it to mlMsh;
- * add in mlMsh uniform refined level-meshes;
- * define the multilevel-solution object mlSol associated to mlMsh;
- * add in mlSol different types of finite element solution variables;
- * initialize the solution varables;
- * define vtk and gmv writer objects associated to mlSol;
- * print vtk and gmv binary-format files in ./output directory.
- **/
-
 #include "FemusInit.hpp"
 #include "MultiLevelProblem.hpp"
 #include "VTKWriter.hpp"
 #include "GMVWriter.hpp"
+#include "LinearImplicitSystem.hpp"
 
 using namespace femus;
 
@@ -29,6 +17,18 @@ double InitalValueP(const std::vector < double >& x) {
 double InitalValueT(const std::vector < double >& x) {
   return x[1];
 }
+
+bool SetBoundaryCondition(const std::vector < double >& x, const char solName[], double& value, const int faceName, const double time) {
+  bool dirichlet = true; //dirichlet
+  value = 0;
+
+  if (faceName == 2)
+    dirichlet = false;
+
+  return dirichlet;
+}
+
+
 
 int main(int argc, char** args) {
 
@@ -57,10 +57,27 @@ int main(int argc, char** args) {
 
   mlSol.Initialize("All");    // initialize all varaibles to zero
 
-  mlSol.Initialize("Thom", InitalValueU);
-  mlSol.Initialize("ThomAdj", InitalValueP);
-  mlSol.Initialize("Tcont", InitalValueT);    // note that this initialization is the same as piecewise constant element
+//   mlSol.Initialize("Thom", InitalValueU);
+//   mlSol.Initialize("ThomAdj", InitalValueP);
+//   mlSol.Initialize("Tcont", InitalValueT);    // note that this initialization is the same as piecewise constant element
  
+  // attach the boundary condition function and generate boundary data
+  mlSol.AttachSetBoundaryConditionFunction(SetBoundaryCondition);
+  mlSol.GenerateBdc("Thom");
+  mlSol.GenerateBdc("ThomAdj");
+  mlSol.GenerateBdc("Tcont");
+
+  // define the multilevel problem attach the mlSol object to it
+  MultiLevelProblem mlProb(&mlSol);
+  
+ // add system  in mlProb as a Linear Implicit System
+  LinearImplicitSystem& system = mlProb.add_system < LinearImplicitSystem > ("Optsys");
+ 
+  system.AddSolutionToSystemPDE("Thom");  
+  system.AddSolutionToSystemPDE("ThomAdj");  
+  system.AddSolutionToSystemPDE("Tcont");  
+  
+  
   
   // print solutions
   std::vector < std::string > variablesToBePrinted;
