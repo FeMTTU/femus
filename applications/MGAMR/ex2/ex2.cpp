24563b6a076c4f3d542dd3a8d93cdcbc12d24b3a
eugenio aulisa <eugenio.aulisa@ttu.edu>
2015-11-28 07:07:48 -0600

    merged with sara forch

--- applications/MGAMR/ex2/ex2.cpp
+++ applications/MGAMR/ex2/ex2.cpp
@@ -52,9 +52,9 @@ bool SetRefinementFlag(const std::vector < double >& x, const int& elemgroupnumb
 
   if (elemgroupnumber == 8 && level < 6) refine = 1;
 
-  if (elemgroupnumber==6 && level<1) refine=1;
-  if (elemgroupnumber==7 && level<2) refine=1;
-  if (elemgroupnumber==8 && level<3) refine=1;
+//   if (elemgroupnumber==6 && level<1) refine=1;
+//   if (elemgroupnumber==7 && level<2) refine=1;
+//   if (elemgroupnumber==8 && level<3) refine=1;
 
   return refine;
 
@@ -85,7 +85,7 @@ int main(int argc, char** args) {
 //   unsigned numberOfSelectiveLevels = 0;
 //   mlMsh.RefineMesh(numberOfUniformLevels , numberOfUniformLevels + numberOfSelectiveLevels, NULL);
 
-  unsigned numberOfUniformLevels = 1;
+  unsigned numberOfUniformLevels = 4;
   unsigned numberOfSelectiveLevels = 3;
   mlMsh.RefineMesh(numberOfUniformLevels + numberOfSelectiveLevels, numberOfUniformLevels , SetRefinementFlag);
 
