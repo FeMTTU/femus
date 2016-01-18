0c64ea6ba87f1f1a5bed9025224c968a1d65f4da
simone.bna <simone.bna@unibo.it>
2016-01-18 19:44:57 +0100

    rm old run fsi scripts and added richter e tukek ones

--- applications/FSI/SteadyState/Benchmarks/run_fsi_richter.sh
+++ applications/FSI/SteadyState/Benchmarks/run_fsi_richter.sh
@@ -8,6 +8,14 @@
 # modify -nrefinement to increase or decrease the number of refinement
 ./fsisteady -input "./input/richter3d.neu" -nrefinement 1 -nlevel 1 -rhof 1000 -muf 1 -rhos 1000 -E 1500000 -ni 0.5 -nnonlin_iter 15 -ic_bdc "../../../../lib64/libfsi_steady_3d_turek_hron_richter_benchmark_bdc.so" -outer_ksp_solver "preonly" -max_outer_solver_iter 1
 
+# Direct solver with ksp_view and residual evaluation
+# modify -nrefinement to increase or decrease the number of refinement
+# ./fsisteady -ksp_view -mat_mumps_icntl_11 1 -input "./input/richter3d.neu" -nrefinement 1 -nlevel 1 -rhof 1000 -muf 1 -rhos 1000 -E 1500000 -ni 0.5 -nnonlin_iter 15 -ic_bdc "../../../../lib64/libfsi_steady_3d_turek_hron_richter_benchmark_bdc.so" -outer_ksp_solver "preonly" -max_outer_solver_iter 1
+
+# Direct solver with ksp_monitor_true_residual
+# modify -nrefinement to increase or decrease the number of refinement
+# ./fsisteady -ksp_monitor_true_residual -input "./input/richter3d.neu" -nrefinement 1 -nlevel 1 -rhof 1000 -muf 1 -rhos 1000 -E 1500000 -ni 0.5 -nnonlin_iter 15 -ic_bdc "../../../../lib64/libfsi_steady_3d_turek_hron_richter_benchmark_bdc.so" -outer_ksp_solver "preonly" -max_outer_solver_iter 1
+
 
 # GMRES-MG-Richardson-ASM-ILU-MUMPS solver
 #./fsisteady -input "./input/turek.neu" -nrefinement 3 -nlevel 3 -rhof 1000 -muf 1 -rhos 1000 -E 1400000 -ni 0.5 -ic_bdc "../../../../lib64/libfsi_steady_2d_turek_hron_benchmark_bdc.so" -outer_ksp_solver "gmres" -max_outer_solver_iter 40
