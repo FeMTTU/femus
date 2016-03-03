216f051b410f9a59c35fd7496cbfe73ef2374484
simone.bna <simone.bna@unibo.it>
2016-03-03 20:10:23 +0100

    fixed fsi2 benchmark parameters

--- applications/FSI/TimeDependent/Benchmarks/run_fsi2_turek.sh
+++ applications/FSI/TimeDependent/Benchmarks/run_fsi2_turek.sh
@@ -6,7 +6,7 @@
 
 # Direct solver
 # modify -nrefinement to increase or decrease the number of refinement
-./fsitimedependent -input "./input/turek.neu" -n_timesteps 500 -autosave_time_interval 50 -nlevel 1 -rhof 1000 -muf 1 -rhos 1000 -E 6000000 -ni 0.5 -ic_bdc "../../../../lib64/libfsi2_td_2d_turek_hron_benchmark_bdc.so" -outer_ksp_solver "preonly" -max_outer_solver_iter 1 -nrefinement 1 -std_output stdOutput.txt > stdOutput.txt
+./fsitimedependent -input "./input/turek.neu" -n_timesteps 500 -autosave_time_interval 50 -nlevel 1 -rhof 1000 -muf 1 -rhos 10000 -E 1500000 -ni 0.5 -ic_bdc "../../../../lib64/libfsi2_td_2d_turek_hron_benchmark_bdc.so" -outer_ksp_solver "preonly" -max_outer_solver_iter 1 -nrefinement 4 -std_output stdOutput.txt > stdOutput.txt
 
 # GMRES-MG-Richardson-ASM-ILU-MUMPS solver
-#./fsitimedependent -input "./input/turek.neu" -n_timesteps 500 -autosave_time_interval 50 -rhof 1000 -muf 1 -rhos 1000 -E 6000000 -ni 0.5 -ic_bdc "../../../../lib64/libfsi2_td_2d_turek_hron_benchmark_bdc.so" -outer_ksp_solver "gmres" -max_outer_solver_iter 60 -ksp_monitor_true_residual -nrefinement 2  -nlevel 2  -std_output turek_mg.txt > turek_mg.txt
+#./fsitimedependent -input "./input/turek.neu" -n_timesteps 500 -autosave_time_interval 50 -rhof 1000 -muf 1 -rhos 10000 -E 1500000 -ni 0.5 -ic_bdc "../../../../lib64/libfsi2_td_2d_turek_hron_benchmark_bdc.so" -outer_ksp_solver "gmres" -max_outer_solver_iter 60 -ksp_monitor_true_residual -nrefinement 2  -nlevel 2  -std_output turek_mg.txt > turek_mg.txt
