b34994a7610fa21a42638f87d1a7b9e1e36d0d2f
scalandr <sara.calandrini@ttu.edu>
2016-04-01 13:13:05 -0500

    second changes to tetrahedral

--- src/fe/Tetrahedral.cpp
+++ src/fe/Tetrahedral.cpp
@@ -166,7 +166,7 @@ namespace femus {
       !(i-1) *( !j     *( !k*4.*(x*t-3.*x*y*z*t)      + !(k-1)*4.*(x*z-3.*x*y*z*t)		      )+
 		!(j-1) *( !k*4.*(x*y-3.*x*y*z*t)					      ) )+
       !(i-2) *( !j     *( !k*((-x+2.*x*x)+3.*x*y*z*t)				      ) )+
-      !(i-7) *( !(j-7) *( !k*27.*x*y*z*t + !(k-1)*27.*x*y*z*t ) ) +
+      !(i-7) *( !(j-7) *( !k*27.*x*y*z*t + !(k-1)*27.*x*y*z*t ) ) + 
       !(i-8) *( !(j-8) *( !(k-1)*27.*x*y*z*t ) ) +
       !(i-9) *( !(j-9) *( !(k-1)*27.*x*y*z*t ) ) +
       !(i-10) *( !(j-10) *( !(k-1)*27.*x*y*z*t ) );
@@ -218,6 +218,25 @@ namespace femus {
       !(i-1) *( !j     *( !k*(-4.)*x	                 )+
 	        !(j-1) *( !k*4.*x		         ) );
   }
+  
+  /*double  tet2::eval_dphidy(const int *I,const double* X) const {
+    
+    const double x=X[0];   const double y=X[1];   const double z=X[2];
+    const int i=I[0];      const int j=I[1];      const int k=I[2];
+    double t=1.-(x+y+z);
+  
+    return
+      !i     *( !j     *( !k*(-4.*t+1.+3.*x*z*(t-y)) + !(k-1)*4.*(-z-3.*x*z*(t-y)) + !(k-2)*3.*x*z*(t-y) )+
+	        !(j-1) *( !k*4.*(t-y-3.*x*z*(t-y))   + !(k-1)*4.*(z-3.*x*z*(t-y))    )+
+	        !(j-2) *( !k*(-1.+4.*y+3.*x*z*(t-y))                  ) )+
+      !(i-1) *( !j     *( !k*4.*(-x-3.*x*z*(t-y)) + !(k-1)*4*(-3.*x*z*(t-y))	                 )+ 
+	        !(j-1) *( !k*4.*(x-3.*x*z*(t-y))		         ) )+
+      !(i-2) *( !j     *( !k*(3.*x*z*(t-y))				      ) )+	        
+      !(i-7) *( !(j-7) *( !k*27.*x*z*(t-y) + !(k-1)*27.*x*z*(t-y) ) ) + 
+      !(i-8) *( !(j-8) *( !(k-1)*27.*x*z*(t-y) ) ) +
+      !(i-9) *( !(j-9) *( !(k-1)*27.*x*z*(t-y) ) ) +
+      !(i-10) *( !(j-10) *( !(k-1)*27.*x*z*(t-y) ) );        
+  }*/
 
 
   double  tet2::eval_dphidz(const int *I,const double* X) const {
@@ -231,6 +250,25 @@ namespace femus {
 		!(j-1) *( !k*(-4.)*y    + !(k-1)*4.*y			     ) )+
       !(i-1) *( !j     *( !k*(-4.)*x    + !(k-1)*4.*x			     ) );
     }
+    
+  /*double  tet2::eval_dphidz(const int *I,const double* X) const { 
+    
+    const double x=X[0];   const double y=X[1];   const double z=X[2];
+    const int i=I[0];      const int j=I[1];      const int k=I[2];
+    double t=1.-(x+y+z);
+  
+    return
+      !i     *( !j     *( !k*(-4.*t+1.+3.*x*y*(t-z)) + !(k-1)*4.*(t-z-3.*x*y*(t-z)) + !(k-2)*(-1+4.*z+3.*x*y*(t-z)) ) +
+		!(j-1) *( !k*4.*(-y-3.*x*y*(t-z))    + !(k-1)*4.*(y-3.*x*y*(t-z))			     ) +
+		!(j-2) *( !k*(3.*x*y*(t-z)) 				      ) ) +
+      !(i-1) *( !j     *( !k*4.*(x-3.*x*y*(t-z))    + !(k-1)*4.*(x-3.*x*y*(t-z))			     ) +
+                !(j-1) *( !k*4.*(-3.*x*y*(t-z))					      ) ) +
+      !(i-2) *( !j     *( !k*(3.*x*y*(t-z))				      ) ) +
+      !(i-7) *( !(j-7) *( !k*27.*x*y*(t-z) + !(k-1)*27.*x*y*(t-z) ) ) + 
+      !(i-8) *( !(j-8) *( !(k-1)*27.*x*y*(t-z) ) ) +
+      !(i-9) *( !(j-9) *( !(k-1)*27.*x*y*(t-z) ) ) +
+      !(i-10) *( !(j-10) *( !(k-1)*27.*x*y*(t-z) ) );
+    }*/  
 
   double  tet2::eval_d2phidx2(const int *I,const double* X) const {
   
@@ -262,10 +300,8 @@ namespace femus {
   
     const int i=I[0]; const int j=I[1];  const int k=I[2];
     return
-      !i    *( !j     *( !k*(4.)  )+
-	       !(j-1) *( !k*(-4.) ) )+
-      !(i-1)*( !j     *( !k*(-4.) )+
-	       !(j-1) *( !k*(4.)  ) );
+      !i    *( !j     *( !k*(4.)  ) + !(j-1) *( !k*(-4.) ) )+
+      !(i-1)*( !j     *( !k*(-4.) ) + !(j-1) *( !k*(4.)  ) );
   }
 
   double  tet2::eval_d2phidydz(const int *I,const double* X) const {
