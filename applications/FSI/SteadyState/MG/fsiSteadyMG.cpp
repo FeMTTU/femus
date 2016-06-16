0d1c9cd6ea5f0a2642aaf056fc1eb0d6248d68a3
scalandr <sara.calandrini@ttu.edu>
2016-06-16 11:42:40 -0500

    bathe_FSI_Sara.neu now works

--- applications/FSI/SteadyState/MG/fsiSteadyMG.cpp
+++ applications/FSI/SteadyState/MG/fsiSteadyMG.cpp
@@ -120,7 +120,7 @@ int main(int argc,char **args) {
     infile = "./input/drum.neu";
   }
   else if(4 == simulation) {
-    infile = "./input/bathe_FSI_Eugenio.neu";
+    infile = "./input/bathe_FSI_Sara.neu";
   }
   else if(5 == simulation){
     infile = "./input/bathe_shell.neu";
