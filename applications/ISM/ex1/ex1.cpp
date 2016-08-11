64383e4b107c274ab35e9302c1fd503137517f70
gcapodag <giacomo.capodaglio@ttu.edu>
2016-08-11 08:38:58 -0500

    fixed some purcaten in  RK 4

--- applications/ISM/ex1/ex1.cpp
+++ applications/ISM/ex1/ex1.cpp
@@ -174,11 +174,12 @@ int main(int argc, char** args) {
         std::cout<< " The coordinates of the marker are " << x[0] << " ," << x[1] << " ," <<x[2]<<std::endl;
         std::cout << " The marker type is " <<  a9QUAD.GetMarkerType() <<std::endl;
 
-       std::vector<double> y = a9QUAD.GetPosition(trial, 16, 0.5);
+       std::vector<double> y = a9QUAD.GetPosition(trial, 13, 0.5);
        for(unsigned i=0; i<2; i++){
 	 std::cout << "y[" << i << "] = " << y[i] << std::endl ;
       }
-
+      std::cout << "errorX = " << sqrt((y[0])*(y[0])) << std::endl;
+      std::cout << "errorY = " << sqrt((1-y[1])*(1-y[1])) <<std::endl;
 
         //print mesh
         MultiLevelSolution mlSol( &mlMsh );
