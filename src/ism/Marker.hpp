c5f6d98b9c5b6fc963459bca44575711cbe55c5a
gcapodag <giacomo.capodaglio@ttu.edu>
2016-09-27 21:06:43 -0500

    added inverseMatrix function and cleaned some code

--- src/ism/Marker.hpp
+++ src/ism/Marker.hpp
@@ -61,13 +61,9 @@ namespace femus {
     std::vector< double > InverseMapping(const unsigned &currentElem, const unsigned &solutionType, const std::vector< double > &x);
     void InverseMapping(const unsigned &iel, const unsigned &solType, 
 			      const std::vector< double > &x, std::vector< double > &xi);
-    std::vector< double > InverseMappingTri(const unsigned &currentElem, const unsigned &solutionType,  std::vector< double > &x);
-    std::vector< double > InverseMappingHex(const unsigned &currentElem, const unsigned &solutionType,  std::vector< double > &x);
-    std::vector< double > InverseMappingTet(const unsigned &currentElem, const unsigned &solutionType,  std::vector< double > &x);
-    std::vector< double > InverseMappingWedge(const unsigned &currentElem, const unsigned &solutionType, std::vector< double > &x);
     
-    unsigned GetNextElement2D(const unsigned &iel,  const unsigned &kel);
-    unsigned GetNextElement3D(const unsigned &iel,  const unsigned &kel);
+    unsigned GetNextElement2D(const unsigned &iel);
+    unsigned GetNextElement3D(const unsigned &iel);
      
     std::vector < double > _x;
     unsigned _solType;
