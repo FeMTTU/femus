689ac8ab6aadd9fcec060dcf3bf5298dd066f19c
gcapodag <giacomo.capodaglio@ttu.edu>
2016-10-03 01:16:10 -0500

    commit to push

--- src/ism/Marker.cpp
+++ src/ism/Marker.cpp
@@ -474,7 +474,7 @@ namespace femus {
 
     for(int k = 0; k < dim; k++) {
       for(int i = 0; i < nDofs; i++) {
-        v[k] -= a[k][i] * phi[i];
+        v[k] -= a[k][i] * phi[i]; // WHY IS THERE PHI AND NOT GRADPHI ???
 
         for(int i1 = 0; i1 < dim; i1++) {
           J[k][i1] += a[k][i] * gradPhi[i][i1];
