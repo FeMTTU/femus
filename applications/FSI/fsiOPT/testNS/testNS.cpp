1106421a03b2cd2b30adeed2c0fa0a63c96b2b76
Sureka Pathmanathan <sureka.pathmanathan@ttu.edu>
2016-10-30 23:25:56 -0500

    testing OPT with I matrix

--- applications/FSI/fsiOPT/testNS/testNS.cpp
+++ applications/FSI/fsiOPT/testNS/testNS.cpp
@@ -63,13 +63,13 @@ int main(int argc, char** args) {
   MultiLevelMesh mlMsh;
   // read coarse level mesh and generate finers level meshes
   double scalingFactor = 1.;
-  mlMsh.ReadCoarseMesh("./input/cube_hex.neu", "seventh", scalingFactor);
-  //mlMsh.ReadCoarseMesh ( "./input/square_quad.neu", "seventh", scalingFactor );
+//   mlMsh.ReadCoarseMesh("./input/cube_hex.neu", "seventh", scalingFactor);
+  mlMsh.ReadCoarseMesh ( "./input/square_quad.neu", "seventh", scalingFactor );
   /* "seventh" is the order of accuracy that is used in the gauss integration scheme
      probably in the furure it is not going to be an argument of this function   */
   unsigned dim = mlMsh.GetDimension();
 
-  unsigned numberOfUniformLevels = 3;
+  unsigned numberOfUniformLevels = 1/*3*/;
   unsigned numberOfSelectiveLevels = 0;
   mlMsh.RefineMesh(numberOfUniformLevels , numberOfUniformLevels + numberOfSelectiveLevels, NULL);
 
