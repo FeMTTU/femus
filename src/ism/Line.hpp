233ca7016dae0c68e19b156c717fd274a50b77ef
gcapodag <giacomo.capodaglio@ttu.edu>
2017-01-22 16:32:43 -0600

    fixed reordering according to procs and elements, to fix printing

--- src/ism/Line.hpp
+++ src/ism/Line.hpp
@@ -40,7 +40,10 @@ namespace femus {
 
         _dim = mesh->GetDimension();
         _size = size;
-        _markerOffset.resize(_nprocs);
+
+        _markerOffset.resize(_nprocs + 1);
+        _markerOffset[_nprocs] = size;
+
         _particles.resize(size);
         _printList.resize(size);
 
@@ -48,21 +51,56 @@ namespace femus {
           particles[j] = new Marker(x[j], markerType[j], mesh, solType, debug);
         }
 
+//         std::cout << "FIRST OF ALL" << std::endl;
+//         for(unsigned i = 0; i < size; i++) {
+//           std::cout << "Particle: " << i << " , " << "Processor: " << particles[i]->GetMarkerProc() << " , "
+//                     << "Element: " << particles[i]->GetMarkerElement() << " " << std::endl;
+//         }
+
 
         //BEGIN reorder the markers by proc
         unsigned counter = 0;
         for(unsigned iproc = 0; iproc < _nprocs; iproc++) {
+          bool markersInProc = false;
+          unsigned  offsetCounter = counter;
           for(unsigned j = 0; j < size; j++) {
             unsigned markerProc = particles[j]->GetMarkerProc();
             if(markerProc == iproc) {
               _particles[counter] = particles[j];
               _printList[j] = counter;
               counter++;
+              markersInProc = true;
             }
           }
-          _markerOffset[iproc] = counter;
+          if(markersInProc == true) {
+            if(iproc == 0) {
+              _markerOffset[iproc] = 0;
+            }
+            else {
+              unsigned firstCounter = 0;
+              for(unsigned jproc = 0; jproc < iproc; jproc++) {
+                if(_markerOffset[jproc] == UINT_MAX) firstCounter++;
+              }
+              if(firstCounter == iproc) _markerOffset[iproc] = 0;
+              else {
+                _markerOffset[iproc] = offsetCounter;
+              }
+            }
+          }
+          else {
+            _markerOffset[iproc] = UINT_MAX;
+          }
+//           std::cout << "_markerOffset[" << iproc << "]= " << _markerOffset[iproc] << std::endl;
         }
         //END reorder the markers by proc
+
+
+//         std::cout << "AFTER THE REORDERING BY PROC" << std::endl;
+//         for(unsigned i = 0; i < size; i++) {
+//           std::cout << "Particle: " << i << " , " << "Processor: " << _particles[i]->GetMarkerProc() << " , "
+//                     << "Element: " << _particles[i]->GetMarkerElement() << " " << std::endl;
+//         }
+
 
         //BEGIN reorder markers also by element
 
@@ -71,29 +109,62 @@ namespace femus {
         }
 
         unsigned* elementList = new unsigned [mesh->GetNumberOfElements()];
+        std::vector < unsigned> printList(size);
+
+        //flags to see if we already ordered by that element, 0 = not considered yet
         for(unsigned iFlag = 0; iFlag < mesh->GetNumberOfElements(); iFlag++) {
           elementList[iFlag] = 0;
         }
 
+        printList = _printList;
+
         for(unsigned iproc = 0; iproc < _nprocs; iproc++) {
 
-          unsigned counter = 1;
+          if(_markerOffset[iproc] != UINT_MAX) {
 
-          for(unsigned jp = _markerOffset[iproc]; jp < _markerOffset[iproc + 1]; jp++) {
+            counter = 0;
 
-            unsigned jel = particles[jp]->GetMarkerElement();
+            unsigned upperBound;
+            bool upperBoundFound = 0;
+            bool finishedProcs = 0;
+            unsigned lproc = iproc + 1;
+            while(upperBoundFound + finishedProcs == 0) {
+              if(_markerOffset[lproc] != UINT_MAX) {
+                upperBound = _markerOffset[lproc];
+                upperBoundFound = 1;
+              }
+              else {
+                if(lproc < _nprocs) lproc ++;
+                else finishedProcs = 1;
+              }
+            }
 
-            if(elementList[jel] != 1) {
+            
+            std::cout << "upperBound =" << upperBound << std::endl;
+                
+            
+            for(unsigned jp = _markerOffset[iproc]; jp < upperBound; jp++) {
 
-              elementList[jel] = 1;
+              unsigned jel = particles[jp]->GetMarkerElement();
 
-              for(unsigned ip = _markerOffset[iproc]; ip < _markerOffset[iproc + 1]; ip++) {
-                unsigned iel = _particles[ip]->GetMarkerElement();
-                if(ip != jp && iel == jel) {
-                  elementList[iel] = 1;
-                  _particles[_markerOffset[iproc] + counter] = particles[ip];
-                  _printList[ip] = _markerOffset[iproc] + counter; //TODO FIX
-                  counter++;
+              if(elementList[jel] == 0) {
+
+                elementList[jel] = 1;
+
+                _particles[_markerOffset[iproc] + counter] = particles[jp];
+                _printList[jp] = _markerOffset[iproc] + counter;
+                counter++;
+
+
+                for(unsigned ip = _markerOffset[iproc]; ip < upperBound; ip++) {
+                  unsigned iel = particles[ip]->GetMarkerElement();
+                  if(ip != jp && iel == jel) {
+                    //std::cout << " jel =" << jel << " , " << "ip = " << ip << " , " << "jp = " << jp <<  std::endl;
+                    elementList[iel] = 1;
+                    _particles[_markerOffset[iproc] + counter] = particles[ip];
+                    _printList[ip] = _markerOffset[iproc] + counter; //TODO FIX
+                    counter++;
+                  }
                 }
               }
             }
@@ -107,14 +178,12 @@ namespace femus {
 //         }
 
 
-        //BEGIN TEST TO SEE IF IT's TRUE,  IT WORKS :)
+//         std::cout << "AFTER THE REORDERING BY ELEM" << std::endl;
+//         for(unsigned i = 0; i < size; i++) {
+//           std::cout << "Particle: " << i << " , " << "Processor: " << _particles[i]->GetMarkerProc() << " , "
+//                     << "Element: " << _particles[i]->GetMarkerElement() << " " << std::endl;
+//         }
 
-        for(unsigned i = 0; i < size; i++) {
-          std::cout << "Particle: " << i << " , " << "Processor: " << _particles[i]->GetMarkerProc() << " , "
-                    << "Element: " << _particles[i]->GetMarkerElement() << " " << std::endl;
-        }
-
-        //END TESTS
 
         _line.resize(1);
         _line[0].resize(size + 1);
@@ -129,6 +198,7 @@ namespace femus {
           delete particles[j];
         }
         delete[] elementList;
+        std::vector < unsigned > ().swap(printList);
 
       };
 
