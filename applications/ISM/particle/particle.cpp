2e37caae41a511e27ae2e4cab42638695cf23447
scalandr <sara.calandrini@ttu.edu>
2017-03-24 19:02:29 -0500

    fixing conflicts

--- applications/ISM/particle/particle.cpp
+++ applications/ISM/particle/particle.cpp
@@ -399,7 +399,7 @@ int main(int argc, char **args)
 
   // time loop parameter
   system.AttachGetTimeIntervalFunction(SetVariableTimeStep);
-  const unsigned int n_timesteps = 500;
+  const unsigned int n_timesteps = 260 /*500*/;
 
   std::vector < std::vector <double> > data(n_timesteps);
 
