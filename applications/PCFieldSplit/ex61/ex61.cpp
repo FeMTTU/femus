ba2e6b62e711290027b17332aaee1789ce9218f9
Guoyi Ke <coyigg@linux-964c.site>
2017-05-10 07:50:26 -0500

    ex61

--- applications/PCFieldSplit/ex61/ex61.cpp
+++ applications/PCFieldSplit/ex61/ex61.cpp
@@ -89,7 +89,7 @@ int main(int argc, char** args) {
   unsigned dim = mlMsh.GetDimension();
 
   unsigned numberOfUniformLevels = 2;
-  unsigned numberOfSelectiveLevels = 4;
+  unsigned numberOfSelectiveLevels = 5;
   mlMsh.RefineMesh(numberOfUniformLevels + numberOfSelectiveLevels, numberOfUniformLevels , SetRefinementFlag);
 //   unsigned numberOfSelectiveLevels = 0;
 //   mlMsh.RefineMesh(numberOfUniformLevels + numberOfSelectiveLevels, numberOfUniformLevels , NULL);
