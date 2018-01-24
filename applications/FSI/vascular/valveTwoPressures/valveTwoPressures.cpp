fe98a1acb29009be3fd972bbd630582dc66dab65
Eugenio Aulisa <eaulisa@pallino.math.ttu.edu>
2018-01-24 15:04:05 -0600

    reduced to 128 iterations

--- applications/FSI/vascular/valveTwoPressures/valveTwoPressures.cpp
+++ applications/FSI/vascular/valveTwoPressures/valveTwoPressures.cpp
@@ -251,7 +251,7 @@ int main(int argc, char** args)
 
   // time loop parameter
   system.AttachGetTimeIntervalFunction(SetVariableTimeStep);
-  const unsigned int n_timesteps = 1024;
+  const unsigned int n_timesteps = 128;
 
   //std::vector < std::vector <double> > data(n_timesteps);
 
