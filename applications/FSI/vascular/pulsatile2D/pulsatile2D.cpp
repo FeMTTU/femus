e049a48e843c59d1cf909691145943665e22fb8d
Eugenio Aulisa <eaulisa@pallino.math.ttu.edu>
2018-02-05 17:01:47 -0600

    5 livelli in pulsatile2D

--- applications/FSI/vascular/pulsatile2D/pulsatile2D.cpp
+++ applications/FSI/vascular/pulsatile2D/pulsatile2D.cpp
@@ -165,7 +165,7 @@ int main(int argc, char **args)
   // ******* Init multilevel mesh from mesh.neu file *******
   unsigned short numberOfUniformRefinedMeshes, numberOfAMRLevels;
 
-  numberOfUniformRefinedMeshes = 4;
+  numberOfUniformRefinedMeshes = 5;
   numberOfAMRLevels = 0;
 
   std::cout << 0 << std::endl;
