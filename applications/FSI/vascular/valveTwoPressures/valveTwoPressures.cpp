7b58ab477ac6208923cb9072b57eeae9e3547c59
Eugenio Aulisa <eaulisa@linux-vdg1.ttu.edu>
2018-02-28 18:26:45 -0600

    fluxes simulations incompressible

--- applications/FSI/vascular/valveTwoPressures/valveTwoPressures.cpp
+++ applications/FSI/vascular/valveTwoPressures/valveTwoPressures.cpp
@@ -66,7 +66,7 @@ int main(int argc, char** args)
 
   E = 260 * 1.0e6; //vein young modulus \\15, 30, 30, 40, 60, 260, 260
   //E = 4.3874951 * 1.0e12;
-  E1 = 1.5 * 1.0e6; //leaflet young modulus \\0.5, 0.8, 1, 1.5, 1.5, 2.2, 1.5
+  E1 = 7.5 * 1.0e6; //leaflet young modulus \\0.5, 0.8, 1, 1.5, 1.5, 2.2, 1.5
   ni1 = 0.5; //0.4999
   
   Parameter par(Lref, Uref);
