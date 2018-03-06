6a18fa3d38f2589dd4406766cea5ce34d6b3f403
Eugenio Aulisa <eaulisa@pallino.math.ttu.edu>
2018-03-06 16:37:26 -0600

    time tests pulsatile2D and 2DFS

--- applications/FSI/vascular/pulsatile2D/pulsatile2D.cpp
+++ applications/FSI/vascular/pulsatile2D/pulsatile2D.cpp
@@ -446,13 +446,13 @@ int main(int argc, char **args)
   std::cout << " TOTAL TIME:\t" << \
           static_cast<double>(clock() - start_time) / CLOCKS_PER_SEC << std::endl;
 	  
-  int  nprocs;	    
-  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-  if(iproc == 0){
-    char stdOutputName[100];
-    sprintf(stdOutputName, "stdoutput_level%d_nprocs%d_turek2D.txt",numberOfUniformRefinedMeshes, nprocs);
-    PrintConvergenceInfo(stdOutputName, numberOfUniformRefinedMeshes, nprocs);
-  } 
+//   int  nprocs;	    
+//   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+//   if(iproc == 0){
+//     char stdOutputName[100];
+//     sprintf(stdOutputName, "stdoutput_level%d_nprocs%d_turek2D.txt",numberOfUniformRefinedMeshes, nprocs);
+//     PrintConvergenceInfo(stdOutputName, numberOfUniformRefinedMeshes, nprocs);
+//   } 
 	  
   return 0;
 }
