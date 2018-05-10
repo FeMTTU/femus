c6f0c5e00dd6da70b7c03c6d9063010e0f352a7d
Eugenio Aulisa <eaulisa@pallino.math.ttu.edu>
2018-05-10 13:22:32 -0500

    trying to fix grid 3 with E8

--- applications/MPM_FEM/ex1/ex1.cpp
+++ applications/MPM_FEM/ex1/ex1.cpp
@@ -57,7 +57,7 @@ int main(int argc, char** args)
 
   MultiLevelMesh mlMsh;
   double scalingFactor = 1.;
-  unsigned numberOfUniformLevels = 4; //for refinement in 3D
+  unsigned numberOfUniformLevels = 3; //for refinement in 3D
   //unsigned numberOfUniformLevels = 1;
   unsigned numberOfSelectiveLevels = 0;
 
@@ -145,7 +145,8 @@ int main(int argc, char** args)
   //BEGIN init particles
   unsigned size = 1;
   std::vector < std::vector < double > > x; // marker
-  double yc = 0.09;  //0.1. (for 3 refinements) 0.075 (for 4) and 0.05  (for 5)
+  double yc = 0.3;  // FOR E = 4.2 * 1.e8 --> 0.115 (for 3 refinements) 0.09 (for 4) and 0.05  (for 5, this one maybe to be changed) 
+                     // FOR E = 4.2 * 1.e6 --> 0.1. (for 3 refinements) 0.075 (for 4) and 0.05  (for 5)
   
   x.resize(size);
   x[0].resize(dim, 0.);
