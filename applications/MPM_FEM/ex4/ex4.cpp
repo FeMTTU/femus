c3a4e68b35c608a1960b8e7aed0cff0a35ba6247
Eugenio Aulisa <eaulisa@linux-vdg1.ttu.edu>
2018-05-14 20:04:06 -0500

    added ex4 (basketball)

--- applications/MPM_FEM/ex4/ex4.cpp
+++ applications/MPM_FEM/ex4/ex4.cpp
@@ -18,33 +18,25 @@
 
 using namespace femus;
 
-double SetVariableTimeStep(const double time) {
+double SetVariableTimeStep(const double time)
+{
   double dt =  0.01;
   return dt;
 }
 
-bool SetBoundaryCondition(const std::vector < double >& x, const char name[], double& value, const int facename, const double time) {
+bool SetBoundaryCondition(const std::vector < double >& x, const char name[], double& value, const int facename, const double time)
+{
   bool test = 1; //dirichlet
   value = 0.;
 
-  if(!strcmp(name, "DY")) {
-    if(3 == facename || 4 == facename) {
-      test = 0;
-      value = 0;
-    }
-  }
-  else if(!strcmp(name, "DX")) {
-    if(2 == facename) {
-      test = 0;
-      value = 0;
-    }
-  }
+  if(1 == facename) test = 0;
 
   return test;
 
 }
 
-int main(int argc, char** args) {
+int main(int argc, char** args)
+{
 
   // init Petsc-MPI communicator
   FemusInit mpinit(argc, args, MPI_COMM_WORLD);
@@ -57,35 +49,35 @@ int main(int argc, char** args) {
 
   double Lref = 1.;
   double Uref = 1.;
-  
+
   //initialize parameters for rolling ball (MPM)
   double rho_MPM = 1000.;
   double nu_MPM = 0.4;
   double E_MPM = 4.2 * 1.e5;
-  
-    //initialize parameters for plate (FEM)
+
+  //initialize parameters for plate (FEM)
   double rho_FEM = 1000.;
   double nu_FEM = 0.4;
-  double E_FEM = 4.2 * 1.e5;
+  double E_FEM = 4.2 * 1.e6;
 
-  beta = 0.3; //was 0.25 
+  beta = 0.3; //was 0.25
   Gamma = 0.5;
-  
+
   Parameter par(Lref, Uref);
 
   // Generate Solid Object
   Solid solidMPM;
   Solid solidFEM;
-  
+
   solidMPM = Solid(par, E_MPM, nu_MPM, rho_MPM, "Neo-Hookean");
   solidFEM = Solid(par, E_FEM, nu_FEM, rho_FEM, "Neo-Hookean");
 
-  mlMsh.ReadCoarseMesh("../input/inclined_plane_coupled_2D.neu", "fifth", scalingFactor);
+  mlMsh.ReadCoarseMesh("../input/basket.neu", "fifth", scalingFactor);
   mlMsh.RefineMesh(numberOfUniformLevels + numberOfSelectiveLevels, numberOfUniformLevels , NULL);
 
   mlMsh.EraseCoarseLevels(numberOfUniformLevels - 1);
   numberOfUniformLevels = 1;
-  
+
   unsigned dim = mlMsh.GetDimension();
 
   MultiLevelSolution mlSol(&mlMsh);
@@ -108,7 +100,7 @@ int main(int argc, char** args) {
   mlSol.Initialize("All");
 
   mlSol.AttachSetBoundaryConditionFunction(SetBoundaryCondition);
-  
+
   mlSol.SetIfFSI(true);
 
   // ******* Set boundary conditions *******
@@ -159,15 +151,16 @@ int main(int argc, char** args) {
   //BEGIN init particles
   unsigned size = 1;
   std::vector < std::vector < double > > x; // marker
-  double yc = 0.15	;  //0.05 for 4 ref, 0.1 for 3 ref, 0.15 for 2 ref
-  
+  double yc = 0.;  // FOR E = 4.2 * 1.e8 --> 0.115 (for 3 refinements) 0.09 (for 4) and 0.05  (for 5, this one maybe to be changed)
+  // FOR E = 4.2 * 1.e6 --> 0.1. (for 3 refinements) 0.075 (for 4) and 0.05  (for 5)
+
   x.resize(size);
   x[0].resize(dim, 0.);
   x[0][1] = yc;
- 
+
   double R = 1.6;
-  double R0 = 1.4; 
-  
+  double R0 = 1.4;
+
   double PI = acos(-1.);
   unsigned NR = 300;
   unsigned NL = NR / (2 * PI);
@@ -190,13 +183,13 @@ int main(int argc, char** args) {
   double MASS = PI * R0 * R0 * rho_MPM;
   size = x.size();
   std::vector < double > mass(x.size(), MASS / x.size()); // uniform marker volume
-  
-  if( fabs(R-R0) > 1.0e-10 ) {
-    
-    double factor = 1.14; 
-    unsigned NL = getNumberOfLayers((R-R0)/DL, factor);
-    std::cout << NL <<std::endl;
-      
+
+  if( fabs(R - R0) > 1.0e-10 ) {
+
+    double factor = 1.14;
+    unsigned NL = getNumberOfLayers((R - R0) / DL, factor);
+    std::cout << NL << std::endl;
+
     double  r = R0;
     for(unsigned i = 1; i <= NL; i++) {
       DL = DL / factor;
@@ -216,14 +209,14 @@ int main(int argc, char** args) {
     }
     size = x.size();
   }
-  
+
   double totalMass = 0;
-  for(unsigned i = 0; i < mass.size(); i++){
+  for(unsigned i = 0; i < mass.size(); i++) {
     totalMass += mass[i];
   }
-  
-  std::cout << totalMass<<" "<< rho_MPM * PI * R * R << std::endl;
-  
+
+  std::cout << totalMass << " " << rho_MPM * PI * R * R << std::endl;
+
   //return 1;
 
   std::vector < MarkerType > markerType;
@@ -232,12 +225,12 @@ int main(int argc, char** args) {
   for(unsigned j = 0; j < size; j++) {
     markerType[j] = VOLUME;
   }
- std::vector < std::vector < std::vector < double > > > line(1);
+  std::vector < std::vector < std::vector < double > > > line(1);
   std::vector < std::vector < std::vector < double > > > line0(1);
 
   unsigned solType = 2;
   linea = new Line(x, mass, markerType, mlSol.GetLevel(numberOfUniformLevels - 1), solType);
-      
+
   //linea->SetParticlesMass(MASS/rhos, rhos);
   //linea->ScaleParticleMass(scale);
 
@@ -245,8 +238,8 @@ int main(int argc, char** args) {
   PrintLine(DEFAULT_OUTPUTDIR, line0, false, 0);
   linea->GetParticlesToGridMaterial();
 
-  //END init particles 
-  
+  //END init particles
+
   // ******* Print solution *******
   mlSol.SetWriter(VTK);
 
@@ -263,7 +256,7 @@ int main(int argc, char** args) {
   mlSol.GetWriter()->SetDebugOutput(true);
   mlSol.GetWriter()->Write(DEFAULT_OUTPUTDIR, "biquadratic", print_vars, 0);
 
-  double theta = PI / 4;
+  double theta = 0;
   gravity[0] = 9.81 * sin(theta);
   gravity[1] = -9.81 * cos(theta);
 
@@ -279,9 +272,9 @@ int main(int argc, char** args) {
     mlSol.GetWriter()->Write(DEFAULT_OUTPUTDIR, "biquadratic", print_vars, time_step);
 
     GridToParticlesProjection(ml_prob, *linea);
-    
-   
-    
+
+
+
 
     linea->GetLine(line[0]);
     PrintLine(DEFAULT_OUTPUTDIR, line, false, time_step);
