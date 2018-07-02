93e181743e6e8d3c4c274059aff16302e1bd64f3
gcapodag <giacomo.capodaglio@ttu.edu>
2018-07-02 00:14:57 -0500

    running tests

--- applications/UQ/include/sgfem_assembly.hpp
+++ applications/UQ/include/sgfem_assembly.hpp
@@ -9,7 +9,7 @@ unsigned pIndex = 5;
 unsigned qIndex = 3;
 
 int numberOfEigPairs = 2; //dimension of the stochastic variable
-double stdDeviationInput = 0.2;  //standard deviation of the normal distribution (it is the same as the standard deviation of the covariance function in GetEigenPair)
+double stdDeviationInput = 0.3;  //standard deviation of the normal distribution (it is the same as the standard deviation of the covariance function in GetEigenPair)
 double amin = 1. / 100; // for the KL expansion
 std::vector < std::pair<double, double> > eigenvalues(numberOfEigPairs);
 //END Stochastic Input Parameters
