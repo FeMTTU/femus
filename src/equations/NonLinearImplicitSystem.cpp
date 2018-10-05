6eb5192894ee1a2b5e2327d5e30cb3055a2b0bcf
Giorgio Bornia <giorgio.bornia@ttu.edu>
2018-10-05 15:38:48 -0500

    Removed weird condition in nonlinear stopping criterion

--- src/equations/NonLinearImplicitSystem.cpp
+++ src/equations/NonLinearImplicitSystem.cpp
@@ -74,7 +74,7 @@ namespace femus {
                 "  ** Eps_l2norm= " << L2normEps << "  ** Sol_l2norm= " << L2normSol << "  ** Res_l2norm= " << L2normRes << std::endl;
       nonLinearEps = (nonLinearEps > L2normEpsDividedSol) ? nonLinearEps : L2normEpsDividedSol;
       
-      if((L2normEpsDividedSol < _max_nonlinear_convergence_tolerance || L2normEps < absMinNonlinearEps || L2normSol < absMinNormSol || ( (k+1) % 3 == 0 )) && conv == true) {
+      if(( L2normEpsDividedSol < _max_nonlinear_convergence_tolerance || L2normEps < absMinNonlinearEps || L2normSol < absMinNormSol ) && conv == true) {
         conv = true;
       }
       else {
