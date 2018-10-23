35e96b7baa9a1eea3d06a626b0e21601546d0e0f
Eugenio Aulisa <eaulisa@linux-0t1o.ttu.edu>
2018-10-23 09:16:28 -0500

    working on c++

--- applications/tutorial/ex11/ex11.cpp
+++ applications/tutorial/ex11/ex11.cpp
@@ -11,18 +11,114 @@ using namespace femus;
 
 adouble f(const adouble x[2]);
 
+
+double * AllocateMemory (const unsigned &n){
+    
+ double *p = new double [n];
+ 
+ return p;
+    
+}
+
+ void swp(unsigned &i, unsigned &j){
+   unsigned temp = i;
+   i = j;
+   j = temp;
+   
+   std::cout << i << " " << j <<std::endl;
+};
+
+
+ void swpp(unsigned *i, unsigned *j){
+   unsigned temp = *i;
+   *i = *j;
+   *j = temp;
+   
+   std::cout << *i << " " << *j <<std::endl;
+};
+
 int main(int argc, char** args)
 {
     // init Petsc-MPI communicator
-    FemusInit mpinit(argc, args, MPI_COMM_WORLD);
-    adept::Stack& s = FemusInit::_adeptStack;
-    adouble x[2]={1.0,2.0};
-    s.new_recording();
-    adouble y=f(x);
-    y.set_gradient(1.0);
-    s.compute_adjoint();
-    cout << y.value() << endl;
-    return 0;
+ //   FemusInit mpinit(argc, args, MPI_COMM_WORLD);
+//     adept::Stack& s = FemusInit::_adeptStack;
+//     adouble x[2]={1.0,2.0};
+//     s.new_recording();
+//     adouble y=f(x);
+//     y.set_gradient(1.0);
+//     s.compute_adjoint();
+//     cout << y.value() << endl;
+//     return 0;
+    
+    unsigned n1;
+    unsigned n2;
+    
+    std::cout << " enter n1 "<<std::endl;
+    std::cin >> n1;
+    
+    if(n1<1) abort();
+    
+    std::cout << " enter n2 "<<std::endl;
+    std::cin >> n2;
+    
+    if(n2<1) abort();
+    
+    
+    
+    double **A;
+    double *Amemory;
+    
+    Amemory = AllocateMemory(n1*n2);
+    
+    
+    
+    A = new double * [n1];
+    
+    for(unsigned i = 0; i < n1; i++){
+      A[i] = &Amemory[0] + i * n2;  
+    }
+    
+    for(unsigned i = 0; i<n1; i++){
+      for(unsigned j = 0; j<n2; j++){
+        A[i][j] = i * n2 + j;    
+      }     
+    }
+    
+    double *p = Amemory;
+    for(unsigned i = 0; i<n1; i++){
+      for(unsigned j = 0; j<n2; j++){
+        std::cout << *(p + i * n2 +j ) << " ";
+      }
+      std::cout << std::endl;
+    }
+    
+    
+    p = Amemory;
+    for(unsigned i = 0; i<n1; i++){
+      for(unsigned j = 0; j<n2; j++){
+        std::cout << *(p++) << " ";
+      }
+      std::cout << std::endl;
+    }
+    
+    
+    
+    
+    
+    delete [] A;
+    delete [] Amemory;
+    
+    
+    unsigned i = 5, j=6;
+    swp(i,j);
+    
+    std::cout << i << " " << j <<std::endl;
+    
+    swpp(&i,&j);
+    
+    std::cout << i << " " << j <<std::endl;
+    
+    
 }
 
 adouble f(const adouble x[2]){
