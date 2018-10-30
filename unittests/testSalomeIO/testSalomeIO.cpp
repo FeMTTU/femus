516576dc2d35f826754263afa31d7f6b2a15d173
Giorgio Bornia <giorgio.bornia@ttu.edu>
2018-10-29 19:23:22 -0500

    Added files for boundary reading

--- unittests/testSalomeIO/testSalomeIO.cpp
+++ unittests/testSalomeIO/testSalomeIO.cpp
@@ -3,7 +3,7 @@
 #include "FemusInit.hpp"
 #include "MultiLevelMesh.hpp"
 #include "WriterEnum.hpp"
-#include "MultiLevelProblem.hpp"
+#include "MultiLevelSolution.hpp"
 
 using namespace femus;
 
@@ -14,7 +14,7 @@ int main(int argc,char **args) {
 
   FemusInit init(argc,args,MPI_COMM_WORLD);
 
-  std::string med_file = "FourQuad9.med";
+  std::string med_file = "FourQuad9_boundaries.med";
   std::ostringstream mystream; mystream << "./" << DEFAULT_INPUTDIR << "/" << med_file;
   const std::string infile = mystream.str();
 
@@ -29,17 +29,24 @@ int main(int argc,char **args) {
   MultiLevelSolution ml_sol(&ml_msh);
 
   // add variables to ml_sol
-  ml_sol.AddSolution("U", LAGRANGE, FIRST);
+  ml_sol.AddSolution("u", LAGRANGE, FIRST);
+  ml_sol.Initialize("All"); 
+  
+  std::vector < std::string > variablesToBePrinted;
+  variablesToBePrinted.push_back("all");
+
+  
   ml_sol.SetWriter(VTK);
-  ml_sol.GetWriter()->SetDebugOutput(true);
-  ml_sol.GetWriter()->Write(DEFAULT_OUTPUTDIR,"biquadratic");
+  ml_sol.GetWriter()->SetDebugOutput(true);  //false: only Sol; true: adds EpsSol, ResSol, BdcSol
+  ml_sol.GetWriter()->Write(DEFAULT_OUTPUTDIR,"biquadratic",variablesToBePrinted);
   ml_sol.SetWriter(GMV);
-  ml_sol.GetWriter()->SetDebugOutput(true);
-  ml_sol.GetWriter()->Write(DEFAULT_OUTPUTDIR,"biquadratic");
-  ml_sol.SetWriter(XDMF);
-  ml_sol.GetWriter()->SetDebugOutput(true);
-  ml_sol.GetWriter()->Write(DEFAULT_OUTPUTDIR,"biquadratic");
+  ml_sol.GetWriter()->SetDebugOutput(true);  //false: only Sol; true: adds EpsSol, ResSol, BdcSol
+  ml_sol.GetWriter()->Write(DEFAULT_OUTPUTDIR,"biquadratic",variablesToBePrinted);
+  ml_sol.SetWriter(XDMF); 
+  ml_sol.GetWriter()->SetDebugOutput(true);  //false: only Sol; true: adds EpsSol, ResSol, BdcSol
+  ml_sol.GetWriter()->Write(DEFAULT_OUTPUTDIR,"biquadratic",variablesToBePrinted);
 
   
   return 0;
 }
+
