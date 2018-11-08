dae2689c900031971eb6c7fbb36b6ad757999ccd
Eugenio Aulisa <eaulisa@linux-0t1o.ttu.edu>
2018-11-08 14:02:54 -0600

    checking ex1a

--- applications/MPM_FEM/include/mpmFem2.hpp
+++ applications/MPM_FEM/include/mpmFem2.hpp
@@ -395,7 +395,7 @@ void AssembleMPMSys(MultiLevelProblem& ml_prob) {
           { {2,1,5}, {3,0,7}, {6,4,8}},
           { {3,2,6}, {0,1,4}, {7,5,8}} };
                 
-        double neumannFactor =  0.5 * (*mysolution->_Sol[indexSolNF])(iel);        
+        double neumannFactor =  1.0 * (*mysolution->_Sol[indexSolNF])(iel);        
         if( neumannFactor > 1.0e-10 ){
           
           for(unsigned iface = 0; iface < 4; iface++ ){
@@ -453,7 +453,7 @@ void AssembleMPMSys(MultiLevelProblem& ml_prob) {
         
       for(int k = 0; k < dim; k++) {
         for(unsigned i = 0; i < nDofs; i++) {
-          SolDp[k] += phi[i] * solD1[k][i];
+          SolDp[k] += phi[i] * solD[k][i];
           for(int j = 0; j < dim; j++) {
             GradSolDpHat[k][j] +=  gradphiHat[i * dim + j] * solD1[k][i];
           }
@@ -820,7 +820,7 @@ void GridToParticlesProjection(MultiLevelProblem & ml_prob, Line & linea) {
           { {2,1,5}, {3,0,7}, {6,4,8}},
           { {3,2,6}, {0,1,4}, {7,5,8}} };
           
-          double neumannFactor =  0.5 * (*mysolution->_Sol[indexSolNF])(iel);        
+          double neumannFactor =  0. * (*mysolution->_Sol[indexSolNF])(iel);        
           if( neumannFactor > 1.0e-10 ){
                       
             for(unsigned iface = 0; iface < 4; iface++ ){
@@ -860,7 +860,7 @@ void GridToParticlesProjection(MultiLevelProblem & ml_prob, Line & linea) {
         //update displacement and acceleration
         for(int i = 0; i < dim; i++) {
           for(unsigned inode = 0; inode < nDofs; inode++) {
-            solDp[i] += phiHat[inode] * solD1[i][inode];
+            solDp[i] += phiHat[inode] * solD[i][inode];
           }
         }
         
