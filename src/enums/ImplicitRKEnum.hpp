5a50528d780bb0d6d87365262c9a7c459990b580
Eugenio Aulisa <eugenio.aulisa@ttu.edu>
2019-02-07 15:52:46 -0600

    fixed conflicts

---
+++ src/enums/ImplicitRKEnum.hpp
@@ -0,0 +1,11 @@
+#ifndef __femus_enums_ImplicitRKEnum_hpp__
+#define __femus_enums_ImplicitRKEnum_hpp__
+
+enum ImplicitRKScheme {
+    LEGENDRE1 = 0,
+    LEGENDRE2,
+    LEGENDRE3,
+    NORSET3
+};
+
+#endif
