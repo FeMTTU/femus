7937974270eabf5bb2713c7f7521d4182321a654
gcapodag <gcapodaglio@fsu.edu>
2019-02-09 01:18:24 -0700

    still testing

--- applications/NonLocal/ex1/ex1.cpp
+++ applications/NonLocal/ex1/ex1.cpp
@@ -55,7 +55,8 @@ int main ( int argc, char** argv )
 //     mlMsh.ReadCoarseMesh ( "../input/nonlocal_boundary_test.neu", "second", scalingFactor );
 //     mlMsh.ReadCoarseMesh ( "../input/interface.neu", "second", scalingFactor );
 //     mlMsh.ReadCoarseMesh ( "../input/maxTest1.neu", "second", scalingFactor );
-    mlMsh.ReadCoarseMesh ( "../input/martaTest1.neu", "second", scalingFactor );
+        mlMsh.ReadCoarseMesh ( "../input/maxTest2.neu", "second", scalingFactor );
+//     mlMsh.ReadCoarseMesh ( "../input/martaTest1.neu", "second", scalingFactor );
 //     mlMsh.ReadCoarseMesh ( "../input/martaTest2.neu", "second", scalingFactor );
 //         mlMsh.ReadCoarseMesh ( "../input/martaTest3.neu", "second", scalingFactor );
 //     mlMsh.ReadCoarseMesh ( "../input/trial1.neu", "second", scalingFactor );
