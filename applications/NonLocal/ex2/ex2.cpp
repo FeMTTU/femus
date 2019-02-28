f2ec3596477bf8db234ba490870c5e563ffb9684
eaulisa <eugenio.aulisa@ttu.edu>
2019-02-28 08:04:54 -0600

    working on the kernel

--- applications/NonLocal/ex2/ex2.cpp
+++ applications/NonLocal/ex2/ex2.cpp
@@ -54,7 +54,7 @@ bool SetBoundaryCondition ( const std::vector < double >& x, const char SolName[
     return dirichlet;
 }
 
-unsigned numberOfUniformLevels = 4;
+unsigned numberOfUniformLevels = 3;
 
 int main ( int argc, char** argv )
 {
