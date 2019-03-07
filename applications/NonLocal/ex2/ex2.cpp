0e44ec344c9b3cc4ec451008aecc0ee777ec8f63
gcapodag <gcapodaglio@fsu.edu>
2019-03-07 12:14:20 -0700

    split nonlocal and local assembly

--- applications/NonLocal/ex2/ex2.cpp
+++ applications/NonLocal/ex2/ex2.cpp
@@ -27,8 +27,8 @@ double InitalValueU ( const std::vector < double >& x )
 //     return x[0];
 //     return x[0] * x[0];
 //     return x[0] * x[0] * x[0] + x[1] * x[1] * x[1];
-    return x[0] * x[0] * x[0] * x[0] + 0.1 * x[0] * x[0]; //this is x^4 + delta x^2
-//     return x[0] * x[0] * x[0] * x[0]; //this is x^4
+//     return x[0] * x[0] * x[0] * x[0] + 0.1 * x[0] * x[0]; //this is x^4 + delta x^2
+    return x[0] * x[0] * x[0] * x[0]; //this is x^4
 //        return 2 * x[0] + x[0] * x[0] * x[0] * x[0] * x[0]; //this is 2x + x^5
 }
 
@@ -42,8 +42,8 @@ bool SetBoundaryCondition ( const std::vector < double >& x, const char SolName[
 //     value = x[0];
 //     value = x[0] * x[0];
 //     value = x[0] * x[0] * x[0] + x[1] * x[1] * x[1];
-    value = x[0] * x[0] * x[0] * x[0] + 0.1 * x[0] * x[0]; //this is x^4 + delta x^2
-//     value = x[0] * x[0] * x[0] * x[0];
+//     value = x[0] * x[0] * x[0] * x[0] + 0.1 * x[0] * x[0]; //this is x^4 + delta x^2
+    value = x[0] * x[0] * x[0] * x[0];
 //        value =  2 * x[0] + x[0] * x[0] * x[0] * x[0] * x[0]; //this is 2x + x^5
 
     if ( facename == 2 ) {
@@ -54,7 +54,7 @@ bool SetBoundaryCondition ( const std::vector < double >& x, const char SolName[
     return dirichlet;
 }
 
-unsigned numberOfUniformLevels = 4;
+unsigned numberOfUniformLevels = 1;
 
 int main ( int argc, char** argv )
 {
@@ -71,11 +71,11 @@ int main ( int argc, char** argv )
 //     mlMsh.ReadCoarseMesh ( "../input/maxTest2.neu", "second", scalingFactor );
 //         mlMsh.ReadCoarseMesh ( "../input/maxTest3.neu", "second", scalingFactor );
 //     mlMsh.ReadCoarseMesh ( "../input/maxTest4.neu", "second", scalingFactor );
-        mlMsh.ReadCoarseMesh ( "../input/maxTest5.neu", "eighth", scalingFactor );
+    mlMsh.ReadCoarseMesh ( "../input/maxTest5.neu", "fifth", scalingFactor );
 //     mlMsh.ReadCoarseMesh ( "../input/maxTest6.neu", "second", scalingFactor );
 //     mlMsh.ReadCoarseMesh ( "../input/maxTest7.neu", "second", scalingFactor );
 //     mlMsh.ReadCoarseMesh ( "../input/maxTest2Continuous.neu", "second", scalingFactor );
-     //mlMsh.ReadCoarseMesh ( "../input/martaTest0.neu", "second", scalingFactor );
+    //mlMsh.ReadCoarseMesh ( "../input/martaTest0.neu", "second", scalingFactor );
 //      mlMsh.ReadCoarseMesh ( "../input/martaTest1.neu", "second", scalingFactor );
 //    mlMsh.ReadCoarseMesh ( "../input/martaTest2.neu", "second", scalingFactor );
 //     mlMsh.ReadCoarseMesh ( "../input/martaTest3.neu", "second", scalingFactor );
@@ -94,18 +94,22 @@ int main ( int argc, char** argv )
 
     // add variables to mlSol
     mlSol.AddSolution ( "u", LAGRANGE, FIRST, 2 );
-    
+
+    mlSol.AddSolution ( "u_local", LAGRANGE, FIRST, 2 );
+
     mlSol.AddSolution ( "u_exact", LAGRANGE, FIRST, 2 );
 
     mlSol.Initialize ( "All" );
 
-    mlSol.Initialize("u_exact", InitalValueU);
+    mlSol.Initialize ( "u_exact", InitalValueU );
 
     mlSol.AttachSetBoundaryConditionFunction ( SetBoundaryCondition );
 
     // ******* Set boundary conditions *******
     mlSol.GenerateBdc ( "All" );
 
+
+    //BEGIN assemble and solve nonlocal problem
     MultiLevelProblem ml_prob ( &mlSol );
 
     // ******* Add FEM system to the MultiLevel problem *******
@@ -143,7 +147,48 @@ int main ( int argc, char** argv )
 
     system.MGsolve();
 
+    //END assemble and solve nonlocal problem
+
+    //BEGIN assemble and solve local problem
+    MultiLevelProblem ml_prob2 ( &mlSol );
+
+    // ******* Add FEM system to the MultiLevel problem *******
+    LinearImplicitSystem& system2 = ml_prob2.add_system < LinearImplicitSystem > ( "Local" );
+    system2.AddSolutionToSystemPDE ( "u_local" );
+
+    // ******* System FEM Assembly *******
+    system2.SetAssembleFunction ( AssembleLocalSys );
+    system2.SetMaxNumberOfLinearIterations ( 1 );
+    // ******* set MG-Solver *******
+    system2.SetMgType ( V_CYCLE );
+
+    system2.SetAbsoluteLinearConvergenceTolerance ( 1.e-50 );
+
+    system2.SetNumberPreSmoothingStep ( 1 );
+    system2.SetNumberPostSmoothingStep ( 1 );
+
+    // ******* Set Preconditioner *******
+    system2.SetMgSmoother ( GMRES_SMOOTHER );
+
+    system2.init();
+
+    // ******* Set Smoother *******
+    system2.SetSolverFineGrids ( GMRES );
+
+    system2.SetPreconditionerFineGrids ( ILU_PRECOND );
+
+    system2.SetTolerances ( 1.e-20, 1.e-20, 1.e+50, 100 );
+
+// ******* Solution *******
+
+    system2.MGsolve();
+
+    //END assemble and solve local problem
+
+
+    //BEGIN compute errors
     GetL2Norm ( ml_prob );
+    //END compute errors
 
     // ******* Print solution *******
     mlSol.SetWriter ( VTK );
@@ -151,7 +196,7 @@ int main ( int argc, char** argv )
     print_vars.push_back ( "All" );
     mlSol.GetWriter()->SetDebugOutput ( true );
     mlSol.GetWriter()->Write ( DEFAULT_OUTPUTDIR, "biquadratic", print_vars, 0 );
-    
+
     return 0;
 
 } //end main
@@ -171,9 +216,13 @@ void GetL2Norm ( MultiLevelProblem& ml_prob )
 
     unsigned xType = 2; // get the finite element type for "x", it is always 2 (LAGRANGE QUADRATIC)
 
-    double local_norm2 = 0.;
+    double error_solExact_norm2 = 0.;
 
-    double sol_norm2 = 0.;
+    double error_solLocal_norm2 = 0.;
+
+    double solNonlocal_norm2 = 0.;
+
+    double solLocal_norm2 = 0.;
 
     double sol_exact_norm2 = 0.;
 
@@ -181,6 +230,8 @@ void GetL2Norm ( MultiLevelProblem& ml_prob )
     soluIndex = mlSol->GetIndex ( "u" );
     unsigned soluType = mlSol->GetSolutionType ( soluIndex );
 
+    unsigned soluIndexLocal;
+    soluIndexLocal = mlSol->GetIndex ( "u_local" );
 
     unsigned    iproc = msh->processor_id();
 
@@ -197,10 +248,12 @@ void GetL2Norm ( MultiLevelProblem& ml_prob )
         }
 
         vector < double >  soluNonLoc ( nDofu );
+        vector < double >  soluLoc ( nDofu );
 
         for ( unsigned i = 0; i < nDofu; i++ ) {
             unsigned solDof = msh->GetSolutionDof ( i, iel, soluType );
             soluNonLoc[i] = ( *sol->_Sol[soluIndex] ) ( solDof );
+            soluLoc[i] = ( *sol->_Sol[soluIndexLocal] ) ( solDof );
         }
 
         for ( unsigned i = 0; i < nDofx; i++ ) {
@@ -220,12 +273,14 @@ void GetL2Norm ( MultiLevelProblem& ml_prob )
             // *** get gauss point weight, test function and test function partial derivatives ***
             msh->_finiteElement[ielGeom][soluType]->Jacobian ( x1, ig, weight, phi, phi_x );
             double soluNonLoc_gss = 0.;
+            double soluLoc_gss = 0.;
             double exactSol_gss_x = 0.;
             double exactSol_gss_y = 0.;
 
 
             for ( unsigned i = 0; i < nDofu; i++ ) {
                 soluNonLoc_gss += phi[i] * soluNonLoc[i];
+                soluLoc_gss += phi[i] * soluLoc[i];
                 exactSol_gss_x += phi[i] * x1[0][i]; // this is x at the Gauss point
 //                 exactSol_gss_y += phi[i] * x1[1][i]; // this is y at the Gauss point
             }
@@ -240,13 +295,17 @@ void GetL2Norm ( MultiLevelProblem& ml_prob )
 //             exactSol_gss_x = exactSol_gss_x * exactSol_gss_x * exactSol_gss_x * exactSol_gss_x; // this is x^4
 
 //             exactSol_gss_x = 2 * exactSol_gss_x  + exactSol_gss_x * exactSol_gss_x * exactSol_gss_x * exactSol_gss_x * exactSol_gss_x ; // this is 2x + x^5
-            
 
-            local_norm2 += ( soluNonLoc_gss - exactSol_gss_x ) * ( soluNonLoc_gss - exactSol_gss_x ) * weight;
 
-//             local_norm2 += (soluNonLoc_gss -  (exactSol_gss_x + exactSol_gss_y)) * (soluNonLoc_gss -  (exactSol_gss_x + exactSol_gss_y)) * weight; //error L2 norm of x^3 + y^3
+            error_solExact_norm2 += ( soluNonLoc_gss - exactSol_gss_x ) * ( soluNonLoc_gss - exactSol_gss_x ) * weight;
 
-            sol_norm2 += soluNonLoc_gss * soluNonLoc_gss * weight;
+            //             error_solExact_norm2 += (soluNonLoc_gss -  (exactSol_gss_x + exactSol_gss_y)) * (soluNonLoc_gss -  (exactSol_gss_x + exactSol_gss_y)) * weight; //error L2 norm of x^3 + y^3
+
+            error_solLocal_norm2 += ( soluNonLoc_gss - soluLoc_gss ) * ( soluNonLoc_gss - soluLoc_gss ) * weight;
+
+            solNonlocal_norm2 += soluNonLoc_gss * soluNonLoc_gss * weight;
+
+            solLocal_norm2 += soluLoc_gss * soluLoc_gss * weight;
 
 //             sol_exact_norm2 += (exactSol_gss_x + exactSol_gss_y) * (exactSol_gss_x + exactSol_gss_y) * weight; //L2 norm of x^3 + y^3
 
@@ -255,22 +314,34 @@ void GetL2Norm ( MultiLevelProblem& ml_prob )
     }
 
     double norm2 = 0.;
-    MPI_Allreduce ( &local_norm2, &norm2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
+    MPI_Allreduce ( &error_solExact_norm2, &norm2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
     double norm = sqrt ( norm2 );
     std::cout.precision ( 14 );
-    std::cout << "Error L2 norm = " << norm << std::endl;
+    std::cout << "L2 norm of ERROR wrt EXACT soln = " << norm << std::endl;
 
     norm2 = 0.;
-    MPI_Allreduce ( &sol_norm2, &norm2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
+    MPI_Allreduce ( &error_solLocal_norm2, &norm2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
     norm = sqrt ( norm2 );
     std::cout.precision ( 14 );
-    std::cout << "Sol L2 norm = " << norm << std::endl;
+    std::cout << "L2 norm of ERROR wrt LOCAL soln = " << norm << std::endl;
+
+    norm2 = 0.;
+    MPI_Allreduce ( &solNonlocal_norm2, &norm2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
+    norm = sqrt ( norm2 );
+    std::cout.precision ( 14 );
+    std::cout << "L2 norm of NONLOCAL soln = " << norm << std::endl;
+
+    norm2 = 0.;
+    MPI_Allreduce ( &solLocal_norm2, &norm2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
+    norm = sqrt ( norm2 );
+    std::cout.precision ( 14 );
+    std::cout << "L2 norm of LOCAL soln = " << norm << std::endl;
 
     norm2 = 0.;
     MPI_Allreduce ( &sol_exact_norm2, &norm2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
     norm = sqrt ( norm2 );
     std::cout.precision ( 14 );
-    std::cout << "Sol Exact L2 norm = " << norm << std::endl;
+    std::cout << "L2 norm of EXACT soln = " << norm << std::endl;
 
 
 }
