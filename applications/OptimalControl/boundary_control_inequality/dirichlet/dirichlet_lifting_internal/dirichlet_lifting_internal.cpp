f24ecdb240d713824fb8807b17ce9fd023921b09
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-03-28 10:17:14 -0500

    Rearranged inequality boundary control problems

--- applications/OptimalControl/boundary_control_inequality/dirichlet/dirichlet_lifting_internal/dirichlet_lifting_internal.cpp
+++ applications/OptimalControl/boundary_control_inequality/dirichlet/dirichlet_lifting_internal/dirichlet_lifting_internal.cpp
@@ -6,7 +6,7 @@
 
 #define FACE_FOR_CONTROL 2  //we do control on the right (=2) face
 #define AXIS_DIRECTION_CONTROL_SIDE  1  //change this accordingly to the other variable above
-#include "../elliptic_param.hpp"
+#include "../../param.hpp"
 
 using namespace femus;
 
--- applications/OptimalControl/boundary_control_inequality/neumann/neumann_lifting_internal/neumann_lifting_internal.cpp
+++ applications/OptimalControl/boundary_control_inequality/neumann/neumann_lifting_internal/neumann_lifting_internal.cpp
@@ -7,7 +7,7 @@
 
 #define FACE_FOR_CONTROL 3  //we do control on the right (=2) face
 #define AXIS_DIRECTION_CONTROL_SIDE  0  //change this accordingly to the other variable above
-#include "../elliptic_param.hpp"
+#include "../../param.hpp"
 
 #define SERVICE 1.
 
