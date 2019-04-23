2295c85faa2bef375473e0b97ae9907139971d1d
Giorgio Bornia <giorgio.bornia@ttu.edu>
2019-04-23 14:44:18 -0500

    Initialized VC function

--- applications/tutorial/ex_time/ex_time.cpp
+++ applications/tutorial/ex_time/ex_time.cpp
@@ -48,6 +48,8 @@ using std::endl;
 using namespace femus;
 
 void AssembleMatrixRes(MultiLevelProblem &ml_prob);
+void AssembleMatrixRes_VC(MultiLevelProblem &ml_prob);
+
 
 //-------------------------------------------------------------------------------------------------------------------
 
@@ -154,8 +156,8 @@ int main(int argc,char **args) {
 //    std::string input_file = "Lshape.med";
 //    std::string input_file = "circle_tri6.med";
 //    std::string input_file = "ellipse_tri6.med";
-   std::string input_file = "ellipse_with_hole_tri6.med";
-//    std::string input_file = "interval.med";
+//    std::string input_file = "ellipse_with_hole_tri6.med";
+   std::string input_file = "interval.med";
    std::ostringstream mystream; mystream << "./" << DEFAULT_INPUTDIR << "/" << input_file;
   const std::string infile = mystream.str();
   
@@ -197,7 +199,7 @@ int main(int argc,char **args) {
 
   system.init();  // does it have to stay here or later?
 
-  system.SetAssembleFunction(AssembleMatrixRes);
+  system.SetAssembleFunction(AssembleMatrixRes_VC);
 
   system.SetOuterSolver(PREONLY);
   system.SetSolverFineGrids(PREONLY);
@@ -300,7 +302,7 @@ int main(int argc,char **args) {
 
 
 //------------------------------------------------------------------------------------------------------------
-void AssembleMatrixRes(MultiLevelProblem &ml_prob){
+void AssembleMatrixRes(MultiLevelProblem &ml_prob) {
 
   TransientNonlinearImplicitSystem* mlPdeSys = & ml_prob.get_system<TransientNonlinearImplicitSystem>("Timedep");
   const unsigned level = mlPdeSys->GetLevelToAssemble();
@@ -547,6 +549,255 @@ void AssembleMatrixRes(MultiLevelProblem &ml_prob){
   // ***************** END ASSEMBLY *******************
 }
 
+
+
+//------------------------------------------------------------------------------------------------------------
+void AssembleMatrixRes_VC(MultiLevelProblem &ml_prob) {
+
+  TransientNonlinearImplicitSystem* mlPdeSys = & ml_prob.get_system<TransientNonlinearImplicitSystem>("Timedep");
+  const unsigned level = mlPdeSys->GetLevelToAssemble();
+
+  MultiLevelSolution *ml_sol			      = ml_prob._ml_sol;
+  Solution*	 sol  	                          = ml_sol->GetSolutionLevel(level);
+  LinearEquationSolver*  pdeSys	              = mlPdeSys->_LinSolver[level];
+  const char* pdename                         = mlPdeSys->name().c_str();
+
+  Mesh*		 msh    	   = ml_prob._ml_msh->GetLevel(level);
+  elem*		 myel		   = msh->el;
+  SparseMatrix*	 JAC	   = pdeSys->_KK;
+  NumericVector* RES 	   = pdeSys->_RES;
+
+  // data
+  const unsigned dim = msh->GetDimension();
+  const unsigned max_size = static_cast< unsigned >(ceil(pow(3, dim)));          // conservative: based on line3, quad9, hex27
+  unsigned nel = msh->GetNumberOfElements();
+  unsigned igrid = msh->GetLevel();
+  unsigned iproc = msh->processor_id();
+  
+  // time dep data
+  double dt = mlPdeSys->GetIntervalTime();
+  double theta = 0.5;
+
+  //************** geometry (at dofs and quadrature points) *************************************  
+  vector < vector < double > > coords_at_dofs(dim);
+  unsigned coords_fe_type = BIQUADR_FE; // get the finite element type for "x", it is always 2 (LAGRANGE BIQUADRATIC)
+  for (unsigned i = 0; i < coords_at_dofs.size(); i++)    coords_at_dofs[i].reserve(max_size);
+
+  vector < double > coord_at_qp(dim);
+  
+  //************* shape functions (at dofs and quadrature points) **************************************  
+  const int solType_max = BIQUADR_FE;  //biquadratic
+
+  double weight_qp; // gauss point weight
+  
+  vector < vector < double > > phi_fe_qp(NFE_FAMS);
+  vector < vector < double > > phi_x_fe_qp(NFE_FAMS);
+  vector < vector < double > > phi_xx_fe_qp(NFE_FAMS);
+ 
+  for(int fe=0; fe < NFE_FAMS; fe++) {  
+        phi_fe_qp[fe].reserve(max_size);
+      phi_x_fe_qp[fe].reserve(max_size*dim);
+     phi_xx_fe_qp[fe].reserve(max_size*(3*(dim-1)));
+   }
+
+  
+  //***************************************************  
+  //********* WHOLE SET OF VARIABLES ****************** 
+  //***************************************************  
+  const unsigned int n_unknowns = mlPdeSys->GetSolPdeIndex().size();
+
+  vector < std::string > Solname(n_unknowns);     Solname[0] = "u";
+  vector < unsigned > SolPdeIndex(n_unknowns);
+  vector < unsigned > SolIndex(n_unknowns);
+  vector < unsigned int > SolFEType(n_unknowns);     //FEtype of each MultilevelSolution       
+  vector < unsigned int > Sol_n_el_dofs(n_unknowns); //number of element dofs
+
+  std::fill(Sol_n_el_dofs.begin(), Sol_n_el_dofs.end(), 0);
+
+  for(unsigned ivar=0; ivar < n_unknowns; ivar++) {
+    SolPdeIndex[ivar] = mlPdeSys->GetSolPdeIndex(  Solname[ivar].c_str() );
+       SolIndex[ivar] = ml_sol->GetIndex         ( Solname[ivar].c_str() );
+      SolFEType[ivar] = ml_sol->GetSolutionType  ( SolIndex[ivar]);
+  }
+  
+  //------------ quantities (at quadrature points) ---------------------
+          vector<double>        sol_qp(n_unknowns);
+          vector<double>    sol_old_qp(n_unknowns);
+  vector< vector<double> > sol_grad_qp(n_unknowns);
+  vector< vector<double> > sol_old_grad_qp(n_unknowns);
+    
+  std::fill(sol_qp.begin(), sol_qp.end(), 0.);
+  std::fill(sol_old_qp.begin(), sol_old_qp.end(), 0.);
+  for (unsigned  k = 0; k < n_unknowns; k++) {
+        sol_grad_qp[k].resize(dim);
+        std::fill(sol_grad_qp[k].begin(), sol_grad_qp[k].end(), 0.);
+        sol_old_grad_qp[k].resize(dim);
+        std::fill(sol_old_grad_qp[k].begin(), sol_old_grad_qp[k].end(), 0.);
+    }
+
+  //----------- quantities (at dof objects) ------------------------------
+  vector < vector < double > >     sol_eldofs(n_unknowns);
+  vector < vector < double > >     sol_old_eldofs(n_unknowns);
+  for(int k=0; k<n_unknowns; k++) { sol_eldofs[k].reserve(max_size);
+                                sol_old_eldofs[k].reserve(max_size);
+   }
+  
+  //******** linear system *******************************************  
+  vector < vector < int > > L2G_dofmap(n_unknowns);     for(int i = 0; i < n_unknowns; i++) { L2G_dofmap[i].reserve(max_size); }
+            vector< int >   L2G_dofmap_AllVars; L2G_dofmap_AllVars.reserve( n_unknowns*max_size );
+          
+  vector< vector< double > > Res_el(n_unknowns);
+  vector< vector< vector< double > > > Jac_el(n_unknowns);
+
+   for(int i = 0; i < n_unknowns; i++) Res_el[i].reserve(max_size);
+
+   for(int i = 0; i < n_unknowns; i++) {
+     Jac_el[i].resize(n_unknowns);
+     for(int j = 0; j < n_unknowns; j++) {
+        Jac_el[i][j].reserve(max_size*max_size);
+     }
+   }
+  
+  // Set to zero all the entries of the matrix
+  RES->zero();
+  JAC->zero();
+
+  // *** element loop ***
+  for (int iel = msh->_elementOffset[iproc]; iel < msh->_elementOffset[iproc+1]; iel++) {
+
+    short unsigned ielGeom = msh->GetElementType(iel);    // element geometry type
+    
+    //******************** GEOMETRY ********************* 
+    unsigned nDofx = msh->GetElementDofNumber(iel, coords_fe_type);       // number of coordinate element dofs
+    for (int i = 0; i < dim; i++)  coords_at_dofs[i].resize(nDofx);
+    // local storage of coordinates
+    for (unsigned i = 0; i < nDofx; i++) {
+      unsigned xDof  = msh->GetSolutionDof(i, iel, coords_fe_type);       // global to global mapping between coordinates node and coordinate dof
+
+      for (unsigned jdim = 0; jdim < dim; jdim++) {
+        coords_at_dofs[jdim][i] = (*msh->_topology->_Sol[jdim])(xDof);    // global extraction and local storage for the element coordinates
+      }
+    }
+  //***************************************************  
+    
+  //all vars###################################################################  
+  for (unsigned  k = 0; k < n_unknowns; k++) {
+    unsigned  ndofs_unk = msh->GetElementDofNumber(iel, SolFEType[k]);
+	   Sol_n_el_dofs[k] = ndofs_unk;
+          sol_eldofs[k].resize(ndofs_unk);
+      sol_old_eldofs[k].resize(ndofs_unk);
+          L2G_dofmap[k].resize(ndofs_unk); 
+    for (unsigned i = 0; i < ndofs_unk; i++) {
+            unsigned solDof = msh->GetSolutionDof(i, iel, SolFEType[k]);                    // global to global mapping between solution node and solution dof 
+                                                                                            // via local to global solution node
+           sol_eldofs[k][i] = (*sol->_Sol[SolIndex[k]])(solDof);                            // global extraction and local storage for the solution
+       sol_old_eldofs[k][i] = (*sol->_SolOld[SolIndex[k]])(solDof);                         // This is OLD in TIME, not in nonlinear loop
+           L2G_dofmap[k][i] = pdeSys->GetSystemDof(SolIndex[k], SolPdeIndex[k], i, iel);    // global to global mapping between solution node and pdeSys dof
+      }
+    }
+    
+    
+    unsigned nDof_AllVars = 0;
+    for (unsigned  k = 0; k < n_unknowns; k++) { nDof_AllVars += Sol_n_el_dofs[k]; }
+    // TODO COMPUTE MAXIMUM maximum number of element dofs for one scalar variable
+    int nDof_max    =  0;   
+      for (unsigned  k = 0; k < n_unknowns; k++)     {
+          if(Sol_n_el_dofs[k] > nDof_max)    nDof_max = Sol_n_el_dofs[k];
+       }
+    
+      for(int k = 0; k < n_unknowns; k++) {
+      L2G_dofmap[k].resize(Sol_n_el_dofs[k]);
+
+      Res_el[SolPdeIndex[k]].resize(Sol_n_el_dofs[k]);
+      memset(& Res_el[SolPdeIndex[k]][0], 0., Sol_n_el_dofs[k] * sizeof(double) );
+
+
+      Jac_el[SolPdeIndex[k]][SolPdeIndex[k]].resize(Sol_n_el_dofs[k] * Sol_n_el_dofs[k]);
+      memset(& Jac_el[SolPdeIndex[k]][SolPdeIndex[k]][0], 0., Sol_n_el_dofs[k] * Sol_n_el_dofs[k] * sizeof(double));
+    }
+
+  //all vars###################################################################    
+
+      // *** quadrature loop ***
+      for(unsigned ig = 0; ig < ml_prob.GetQuadratureRule(ielGeom).GetGaussPointsNumber(); ig++) {
+          
+      // *** get gauss point weight, test function and test function partial derivatives ***
+      for(int fe=0; fe < NFE_FAMS; fe++) {
+         msh->_finiteElement[ielGeom][fe]->Jacobian(coords_at_dofs,ig,weight_qp,phi_fe_qp[fe],phi_x_fe_qp[fe],phi_xx_fe_qp[fe]);
+      }
+      //HAVE TO RECALL IT TO HAVE BIQUADRATIC JACOBIAN
+         msh->_finiteElement[ielGeom][coords_fe_type]->Jacobian(coords_at_dofs,ig,weight_qp,phi_fe_qp[coords_fe_type],phi_x_fe_qp[coords_fe_type],phi_xx_fe_qp[coords_fe_type]);
+
+   //========= fill gauss value quantities ==================   
+   std::fill(sol_qp.begin(), sol_qp.end(), 0.);
+   std::fill(sol_old_qp.begin(), sol_old_qp.end(), 0.);
+   for (unsigned  k = 0; k < n_unknowns; k++) { std::fill(sol_grad_qp[k].begin(), sol_grad_qp[k].end(), 0.); 
+                                                std::fill(sol_old_grad_qp[k].begin(), sol_old_grad_qp[k].end(), 0.);
+                                            }
+    
+    for (unsigned  k = 0; k < n_unknowns; k++) {
+	for (unsigned i = 0; i < Sol_n_el_dofs[k]; i++) {
+	                                                         sol_qp[k]    +=     sol_eldofs[k][i] *   phi_fe_qp[SolFEType[k]][i];
+	                                                     sol_old_qp[k]    += sol_old_eldofs[k][i] *   phi_fe_qp[SolFEType[k]][i];
+                   for (unsigned d = 0; d < dim; d++) {      sol_grad_qp[k][d] +=     sol_eldofs[k][i] * phi_x_fe_qp[SolFEType[k]][i * dim + d]; 
+                                                         sol_old_grad_qp[k][d] += sol_old_eldofs[k][i] * phi_x_fe_qp[SolFEType[k]][i * dim + d]; 
+                                     }
+       }        
+    }
+ 
+  //========= fill gauss value quantities ==================
+         
+	// *** phi_i loop ***
+	for(unsigned i = 0; i < nDof_max; i++) {
+
+    //BEGIN RESIDUALS A block ===========================
+	    double Lap_rhs_i = 0.;
+	    double Lap_old_rhs_i = 0.;
+	    for(unsigned d = 0;  d < dim;  d++) {
+	      Lap_rhs_i     += phi_x_fe_qp[SolFEType[0]][i * dim + d] *     sol_grad_qp[0][d];
+	      Lap_old_rhs_i += phi_x_fe_qp[SolFEType[0]][i * dim + d] * sol_old_grad_qp[0][d];
+	    }
+	    Res_el[SolPdeIndex[0]][i] +=  - weight_qp * ( 
+                dt * (
+                     +       theta  * ( Lap_rhs_i     +  nonlin_term_function(sol_qp[0])     * phi_fe_qp[ SolFEType[0] ][i] )     // Laplacian + nonlinear term
+					 + (1. - theta) * ( Lap_old_rhs_i +  nonlin_term_function(sol_old_qp[0]) * phi_fe_qp[ SolFEType[0] ][i] )     // Laplacian + nonlinear term
+                     ) 
+					+ (sol_qp[0] - sol_old_qp[0]) * phi_fe_qp[ SolFEType[0] ][i]        // acceleration
+            );
+    //END RESIDUALS A block ===========================
+
+
+	    // *** phi_j loop ***
+	    for(unsigned j = 0; j<nDof_max; j++) {
+            
+          double Lap_mat_i_j = 0.;
+          for(unsigned d = 0; d < dim; d++) Lap_mat_i_j += phi_x_fe_qp[SolFEType[0]][i * dim + d] *
+                                                           phi_x_fe_qp[SolFEType[0]][j * dim + d];
+
+
+          Jac_el[SolPdeIndex[0]][SolPdeIndex[0]][i * Sol_n_el_dofs[0] + j] += weight_qp * ( dt * ( Lap_mat_i_j  + phi_fe_qp[ SolFEType[0] ][i] * nonlin_term_derivative(  phi_fe_qp[ SolFEType[0] ][j] ) * phi_fe_qp[ SolFEType[0] ][j] )
+                                                                                                                + phi_fe_qp[ SolFEType[0] ][i] * phi_fe_qp[ SolFEType[0] ][j] );
+	      
+  	    }    //end phij loop
+
+        
+    }        //end phii loop
+    
+  }          // end gauss point loop
+
+//--------------------------------------------------------------------------------------------------------
+    //Sum the local matrices/vectors into the Global Matrix/Vector
+    for(unsigned ivar=0; ivar < n_unknowns; ivar++) {
+      RES->add_vector_blocked(Res_el[SolPdeIndex[ivar]],L2G_dofmap[ivar]);
+      JAC->add_matrix_blocked(Jac_el[SolPdeIndex[ivar]][SolPdeIndex[ivar]],L2G_dofmap[ivar],L2G_dofmap[ivar]);
+    }
+    //--------------------------------------------------------------------------------------------------------
+  } //end list of elements loop for each subdomain
+
+  JAC->close();
+  RES->close();
+  // ***************** END ASSEMBLY *******************
+}
   /**
    * 
    * It would be nice to add some code that can check the error in some appropriate norm, or potentially some convergence tests.
