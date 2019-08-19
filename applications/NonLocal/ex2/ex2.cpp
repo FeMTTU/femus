7c3565b8f5945b71ded8beae11c723aed80f6fbf
gcapodag <gcapodaglio@fsu.edu>
2019-08-19 17:43:11 -0600

    trying Richardson

--- applications/NonLocal/ex2/ex2.cpp
+++ applications/NonLocal/ex2/ex2.cpp
@@ -78,7 +78,7 @@ bool SetBoundaryCondition (const std::vector < double >& x, const char SolName[]
   return dirichlet;
 }
 
-unsigned numberOfUniformLevels = 4;
+unsigned numberOfUniformLevels = 2;
 
 int main (int argc, char** argv) {
 
@@ -179,7 +179,8 @@ int main (int argc, char** argv) {
   system.init();
 
   // ******* Set Smoother *******
-  system.SetSolverFineGrids (GMRES);
+  system.SetSolverFineGrids (RICHARDSON);
+//   system.SetRichardsonScaleFactor(0.7);
 
   system.SetPreconditionerFineGrids (ILU_PRECOND);
 
