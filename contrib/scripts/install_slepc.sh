54ee4815e5dee12a48e7e4348658d02c3b941f1c
Andrea Chierici <andrea@mam155.ttu.edu>
2020-02-20 16:50:31 -0600

    Updated petsc/slepc scripts, and created new uq ex2a app

--- contrib/scripts/install_slepc.sh
+++ contrib/scripts/install_slepc.sh
@@ -3,22 +3,34 @@
  
  #a development version of slepc cannot be installed with a release version of slepc
  
+
+echo Install slepc
+
+if test "$1" != "--prefix-external"; then
+echo "The first argument must be --prefix-external"; exit;
+fi 
+ 
+ 
 echo "The PETSC directory is " $PETSC_DIR
 echo "The PETSC architecture is " $PETSC_ARCH
 
 SLEPC_NAME=slepc
 
-REF_DIR=$HOME/software/
+#  SOFTWARE_DIR=$HOME/software/
+SOFTWARE_DIR=`readlink -f $2`
+echo "=========" $SOFTWARE_DIR
 
-cd $REF_DIR
 
-git clone https://bitbucket.org/slepc/slepc
+
+cd $SOFTWARE_DIR
+
+git clone https://gitlab.com/slepc/slepc
 
 cd $SLEPC_NAME
 
-export SLEPC_DIR=$REF_DIR/$SLEPC_NAME
+export SLEPC_DIR=$SOFTWARE_DIR/$SLEPC_NAME
 
-git checkout v3.10.2 -b slepc_current
+# git checkout v3.12.2 -b slepc_current
 
 ./configure
 make
