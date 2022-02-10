a36a668f56a37605b6cfea5bc76a6c2f4bcbe32d
Abubakarr Yillah <abubakarr.yillah@ttu.edu>
2022-02-10 10:42:00 -0600

    2D mesh of a rectangle completed

--- applications/2021_fall/ayillah/ayillah.cpp
+++ applications/2021_fall/ayillah/ayillah.cpp
@@ -69,8 +69,8 @@ bool SetBoundaryCondition(const MultiLevelProblem * ml_prob, const std::vector <
             value = 0.;
         }
         else if (face_name == 4) {
-            dirichlet = false;
-            value = 1. * ( x[0] * x[0]); //Neumann function, here we specify the WHOLE normal derivative, which is a scalar, not each Cartesian component
+            dirichlet = true;
+            value = 0.* ( x[0] * x[0]); //Neumann function, here we specify the WHOLE normal derivative, which is a scalar, not each Cartesian component
         }
 
 
@@ -281,7 +281,7 @@ int main(int argc, char** args) {
     // ======= Mesh  ==================
     std::vector<std::string> mesh_files;
 
-    mesh_files.push_back("Mesh_1_x_dir_neu.med");
+    mesh_files.push_back("parametric_square.med");
 //    mesh_files.push_back("Mesh_2_xy_boundaries_groups_4x4.med");
 //    mesh_files.push_back("Mesh_1_x_all_dir.med");
 //    mesh_files.push_back("Mesh_1_y_all_dir.med");
