dba6685755be75e8ac7149d7158dd3a0f83f7301
student <student@mam001.ttu.edu>
2022-04-11 10:38:59 -0500

    tetrahedral mesh is now working

--- applications/2021_fall/rifat/input/Mesh_Assignment_semi_cylinder_tetrahedron.py
+++ applications/2021_fall/rifat/input/Mesh_Assignment_semi_cylinder_tetrahedron.py
@@ -88,17 +88,89 @@ try:
   pass
 except:
   print('ExportMED() failed. Invalid file name?')
+smesh.SetName(Mesh_1, 'Mesh_1')
+try:
+  Mesh_1.ExportMED(r'/home/student/software/femus/applications/2021_fall/rifat/input/Mesh_Assignment_semi_cylinder_tetrahedron.med',auto_groups=0,version=41,overwrite=1,meshPart=None,autoDimension=1)
+  pass
+except:
+  print('ExportMED() failed. Invalid file name?')
+Mesh_1.ConvertToQuadratic(0)
+smesh.SetName(Mesh_1, 'Mesh_1')
+try:
+  Mesh_1.ExportMED(r'/home/student/software/femus/applications/2021_fall/rifat/input/Mesh_Assignment_semi_cylinder_tetrahedron.med',auto_groups=0,version=41,overwrite=1,meshPart=None,autoDimension=1)
+  pass
+except:
+  print('ExportMED() failed. Invalid file name?')
+Mesh_1.ConvertFromQuadratic()
+Mesh_1.ConvertToQuadratic(0)
+smesh.SetName(Mesh_1, 'Mesh_1')
+try:
+  Mesh_1.ExportMED(r'/home/student/software/femus/applications/2021_fall/rifat/input/Mesh_Assignment_semi_cylinder_tetrahedron.med',auto_groups=0,version=41,overwrite=1,meshPart=None,autoDimension=1)
+  pass
+except:
+  print('ExportMED() failed. Invalid file name?')
+Mesh_2 = smesh.Mesh(Cut_1)
+status = Mesh_2.AddHypothesis(Viscous_Layers_1)
+NETGEN_1D_2D_3D_1 = Mesh_2.Tetrahedron(algo=smeshBuilder.NETGEN_1D2D3D)
+NETGEN_3D_Simple_Parameters_1 = NETGEN_1D_2D_3D_1.Parameters(smeshBuilder.SIMPLE)
+NETGEN_3D_Simple_Parameters_1.SetNumberOfSegments( 10 )
+NETGEN_3D_Simple_Parameters_1.LengthFromEdges()
+NETGEN_3D_Simple_Parameters_1.LengthFromFaces()
+NETGEN_3D_Simple_Parameters_1.SetLocalLength( 0.6 )
+NETGEN_3D_Simple_Parameters_1.SetMaxElementArea( 0.3 )
+NETGEN_3D_Simple_Parameters_1.SetMaxElementVolume( 0.3 )
+isDone = Mesh_2.Compute()
+Mesh_2.ConvertToQuadratic(0)
+Group_1_0_1 = Mesh_2.CreateEmptyGroup( SMESH.FACE, 'Group_1_0' )
+nbAdd = Group_1_0_1.Add( [ 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96 ] )
+[ Group_1_0_1 ] = Mesh_2.GetGroups()
+Group_2_0_1 = Mesh_2.CreateEmptyGroup( SMESH.FACE, 'Group_2_0' )
+nbAdd = Group_2_0_1.Add( [ 23, 24, 25, 26, 27, 28, 29, 30 ] )
+[ Group_1_0_1, Group_2_0_1 ] = Mesh_2.GetGroups()
+Group_3_0_1 = Mesh_2.CreateEmptyGroup( SMESH.FACE, 'Group_1' )
+nbAdd = Group_3_0_1.Add( [ 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58 ] )
+[ Group_1_0_1, Group_2_0_1, Group_3_0_1 ] = Mesh_2.GetGroups()
+Group_3_0_1.SetName( 'Group_3_0' )
+[ Group_1_0_1, Group_2_0_1, Group_3_0_1 ] = Mesh_2.GetGroups()
+Group_4_0_1 = Mesh_2.CreateEmptyGroup( SMESH.FACE, 'Group_1' )
+nbAdd = Group_4_0_1.Add( [ 31, 32, 33, 34, 35, 36, 37, 38 ] )
+[ Group_1_0_1, Group_2_0_1, Group_3_0_1, Group_4_0_1 ] = Mesh_2.GetGroups()
+Group_4_0_1.SetName( 'Group_4_0' )
+smesh.SetName(Mesh_2, 'Mesh_2')
+try:
+  Mesh_2.ExportMED(r'/home/student/software/femus/applications/2021_fall/rifat/input/Mesh_Assignment_semi_cylinder_tetrahedron.med',auto_groups=0,version=41,overwrite=1,meshPart=None,autoDimension=1)
+  pass
+except:
+  print('ExportMED() failed. Invalid file name?')
+Mesh_1.ConvertFromQuadratic()
+[ Group_1_0, Group_2_0, Group_3_0, Group_4_0 ] = Mesh_1.GetGroups()
+status = Mesh_1.RemoveHypothesis(Viscous_Layers_1)
+isDone = Mesh_1.Compute()
+[ Group_1_0, Group_2_0, Group_3_0, Group_4_0 ] = Mesh_1.GetGroups()
+Mesh_1.ConvertToQuadratic(0)
+smesh.SetName(Mesh_1, 'Mesh_1')
+try:
+  Mesh_1.ExportMED(r'/home/student/software/femus/applications/2021_fall/rifat/input/Mesh_Assignment_semi_cylinder_tetrahedron.med',auto_groups=0,version=41,overwrite=1,meshPart=None,autoDimension=0)
+  pass
+except:
+  print('ExportMED() failed. Invalid file name?')
 
 
 ## Set names of Mesh objects
 smesh.SetName(NETGEN_1D_2D_3D.GetAlgorithm(), 'NETGEN 1D-2D-3D')
 smesh.SetName(Group_1_0, 'Group_1_0')
 smesh.SetName(Viscous_Layers_1, 'Viscous Layers_1')
+smesh.SetName(NETGEN_3D_Simple_Parameters_1, 'NETGEN 3D Simple Parameters_1')
 smesh.SetName(NETGEN_3D_Parameters_1, 'NETGEN 3D Parameters_1')
 smesh.SetName(Mesh_1.GetMesh(), 'Mesh_1')
+smesh.SetName(Mesh_2.GetMesh(), 'Mesh_2')
 smesh.SetName(Group_3_0, 'Group_3_0')
 smesh.SetName(Group_2_0, 'Group_2_0')
 smesh.SetName(Group_4_0, 'Group_4_0')
+smesh.SetName(Group_4_0_1, 'Group_4_0')
+smesh.SetName(Group_3_0_1, 'Group_3_0')
+smesh.SetName(Group_2_0_1, 'Group_2_0')
+smesh.SetName(Group_1_0_1, 'Group_1_0')
 
 
 if salome.sg.hasDesktop():
