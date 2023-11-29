a0443f320d1df55be69e41b7300191a0a2386c1a
Karthik Vasudeva <karthik.vasudeva@ttu.edu>
2023-11-29 16:36:58 -0600

    boudary integral for biharmonic working for zero DBC

---
+++ applications/zzz_sandbox/Karthik/coupled_biharmonic/coupled_biharmonic.cpp
@@ -0,0 +1,327 @@
+/** tutorial/Ex3
+ * This example shows how to set and solve the weak form of the nonlinear problem
+ *                     -\Delta^2 u = f(x) \text{ on }\Omega,
+ *            u=0 \text{ on } \Gamma,
+ *      \Delta u=0 \text{ on } \Gamma,
+ * on a box domain $\Omega$ with boundary $\Gamma$,
+ * by using a system of second order partial differential equation.
+ * all the coarse-level meshes are removed;
+ * a multilevel problem and an equation system are initialized;
+ * a direct solver is used to solve the problem.
+ **/
+
+
+
+#include "FemusInit.hpp"
+#include "Files.hpp"
+#include "MultiLevelProblem.hpp"
+#include "MultiLevelSolution.hpp"
+#include "NonLinearImplicitSystem.hpp"
+#include "LinearEquationSolver.hpp"
+#include "VTKWriter.hpp"
+#include "NumericVector.hpp"
+
+//#include "biharmonic_coupled.hpp"
+
+#include "FE_convergence.hpp"
+
+#include "Solution_functions_over_domains_or_mesh_files.hpp"
+
+#include "adept.h"
+
+
+#define LIBRARY_OR_USER   1 //0: library; 1: user
+
+#if LIBRARY_OR_USER == 0
+   #include "01_biharmonic_coupled.hpp"
+   #define NAMESPACE_FOR_BIHARMONIC   femus
+#elif LIBRARY_OR_USER == 1
+   #include "biharmonic_coupled.hpp"
+   #define NAMESPACE_FOR_BIHARMONIC_COUPLED   karthik
+#endif
+
+
+
+using namespace femus;
+
+double GetExactSolutionValue(const std::vector < double >& x) {
+  Domains::square_m05p05::Function_Zero_on_boundary_4 <double> analytical_function_1;
+
+  double FunctionValue = analytical_function_1.value(x);
+  return FunctionValue;
+};
+
+void GetExactSolutionGradient(const std::vector < double >& x, std::vector < double >& solGrad) {
+  Domains::square_m05p05::Function_Zero_on_boundary_4 <double> analytical_function_1;
+  unsigned int m = analytical_function_1.gradient(x).size();
+
+  solGrad[0]  = analytical_function_1.gradient(x)[0];
+  solGrad[1] = analytical_function_1.gradient(x)[1];
+};
+
+
+
+
+
+
+// // // // Solution - BEGIN
+// // //
+// // // double GetExactSolutionValue(const std::vector < double >& x) {
+// // //   double pi = acos(-1.);
+// // //   return cos(pi * x[0]) * cos(pi * x[1]);
+// // // };
+// // //
+// // //
+// // // void GetExactSolutionGradient(const std::vector < double >& x, std::vector < double >& solGrad) {
+// // //   double pi = acos(-1.);
+// // //   solGrad[0]  = -pi * sin(pi * x[0]) * cos(pi * x[1]);
+// // //   solGrad[1] = -pi * cos(pi * x[0]) * sin(pi * x[1]);
+// // // };
+// // //
+// // //
+// // // double GetExactSolutionLaplace(const std::vector < double >& x) {
+// // //   double pi = acos(-1.);
+// // //   return -2.*pi * pi * cos(pi * x[0]) * cos(pi * x[1]);       // - pi*pi*cos(pi*x[0])*cos(pi*x[1]);
+// // // };
+// // //
+// // //
+// // // // Solution - END
+
+
+
+
+bool SetBoundaryCondition_bc_all_dirichlet_homogeneous(const MultiLevelProblem * ml_prob, const std::vector < double >& x, const char SolName[], double& value, const int facename, const double time) {
+
+  bool dirichlet;
+
+  if (!strcmp(SolName, "u")) {
+  dirichlet = true; //dirichlet
+  value = 0.;
+  }
+  if (!strcmp(SolName, "v")) {
+  dirichlet = true; //dirichlet
+  value = 0.;
+  }
+
+  return dirichlet;
+
+}
+
+
+
+
+
+
+int main(int argc, char** args) {
+
+  // init Petsc-MPI communicator
+  FemusInit mpinit(argc, args, MPI_COMM_WORLD);
+
+  // ======= Files - BEGIN  ========================
+  const bool use_output_time_folder = false; // This allows you to run the code multiple times without overwriting. This will generate an output folder each time you run.
+  const bool redirect_cout_to_file = false; // puts the output in a log file instead of the term
+  Files files;
+        files.CheckIODirectories(use_output_time_folder);
+        files.RedirectCout(redirect_cout_to_file);
+
+  // ======= Files - END  ========================
+
+
+    // ======= System Specifics - BEGIN  ==================
+  system_specifics  system_biharmonic_coupled;   //me
+
+  system_biharmonic_coupled._mesh_files.push_back("square_-0p5-0p5x-0p5-0p5_divisions_2x2.med");
+  const std::string relative_path_to_build_directory =  "../../../../";
+  const std::string mesh_file = relative_path_to_build_directory + Files::mesh_folder_path() + "00_salome/02_2d/square/minus0p5-plus0p5_minus0p5-plus0p5/";  system_biharmonic_coupled._mesh_files_path_relative_to_executable.push_back(mesh_file);
+
+  system_biharmonic_coupled._system_name = "Biharmonic";
+  system_biharmonic_coupled._assemble_function = NAMESPACE_FOR_BIHARMONIC_COUPLED :: biharmonic_coupled_equation :: AssembleBilaplaceProblem_AD;
+
+  system_biharmonic_coupled._boundary_conditions_types_and_values             = SetBoundaryCondition_bc_all_dirichlet_homogeneous;
+
+
+
+  Domains::square_01by01::Function_Zero_on_boundary_5<>   system_biharmonic_coupled_function_zero_on_boundary_1;
+  Domains::square_01by01::Function_Zero_on_boundary_5_Laplacian<>   system_biharmonic_coupled_function_zero_on_boundary_1_laplacian;
+  system_biharmonic_coupled._assemble_function_for_rhs   = & system_biharmonic_coupled_function_zero_on_boundary_1_laplacian; //this is the RHS for the auxiliary variable v = -Delta u
+  system_biharmonic_coupled._true_solution_function      = & system_biharmonic_coupled_function_zero_on_boundary_1;
+
+  ///@todo if this is not set, nothing happens here. It is used to compute absolute errors
+    // ======= System Specifics - END ==================
+
+
+
+  // define multilevel mesh
+  MultiLevelMesh mlMsh;
+  // read coarse level mesh and generate finers level meshes
+  double scalingFactor = 1.;
+  const std::string mesh_file_total = system_biharmonic_coupled._mesh_files_path_relative_to_executable[0] + "/" + system_biharmonic_coupled._mesh_files[0];
+  mlMsh.ReadCoarseMesh(mesh_file_total.c_str(), "seventh", scalingFactor);
+
+  unsigned maxNumberOfMeshes = 5;
+
+  std::vector < std::vector < double > > l2Norm;
+  l2Norm.resize(maxNumberOfMeshes);
+
+  std::vector < std::vector < double > > semiNorm;
+  semiNorm.resize(maxNumberOfMeshes);
+
+    std::vector<FEOrder> feOrder;
+    feOrder.push_back(FIRST);
+    feOrder.push_back(SERENDIPITY);
+    feOrder.push_back(SECOND);
+
+
+
+
+
+  for (unsigned i = 0; i < maxNumberOfMeshes; i++) {   // loop on the mesh level
+
+    unsigned numberOfUniformLevels = i + 1;
+    unsigned numberOfSelectiveLevels = 0;
+    mlMsh.RefineMesh(numberOfUniformLevels , numberOfUniformLevels + numberOfSelectiveLevels, NULL);
+
+    // erase all the coarse mesh levels
+    mlMsh.EraseCoarseLevels(numberOfUniformLevels - 1);
+
+    // print mesh info
+    mlMsh.PrintInfo();
+
+    l2Norm[i].resize( feOrder.size() );
+    semiNorm[i].resize( feOrder.size() );
+
+    for (unsigned j = 0; j < feOrder.size(); j++) {   // loop on the FE Order
+
+      // define the multilevel solution and attach the mlMsh object to it
+      MultiLevelSolution mlSol(&mlMsh);
+
+      // add variables to mlSol
+      mlSol.AddSolution("u", LAGRANGE, feOrder[j]);
+      mlSol.AddSolution("v", LAGRANGE, feOrder[j]);
+      mlSol.Initialize("All");
+
+
+      // define the multilevel problem attach the mlSol object to it
+      MultiLevelProblem ml_prob(&mlSol);
+
+      ml_prob.set_app_specs_pointer(& system_biharmonic_coupled);
+      // ======= Problem, Files ========================
+      ml_prob.SetFilesHandler(&files);
+
+      // attach the boundary condition function and generate boundary data
+      mlSol.AttachSetBoundaryConditionFunction( system_biharmonic_coupled._boundary_conditions_types_and_values );
+      mlSol.GenerateBdc("u", "Steady", & ml_prob);
+      mlSol.GenerateBdc("v", "Steady", & ml_prob);
+
+      // add system Biharmonic in ml_prob as a Linear Implicit System
+      NonLinearImplicitSystem& system = ml_prob.add_system < NonLinearImplicitSystem > (system_biharmonic_coupled._system_name);
+
+      // add solution "u" to system
+      system.AddSolutionToSystemPDE("u");
+      system.AddSolutionToSystemPDE("v");
+
+      // attach the assembling function to system
+      system.SetAssembleFunction( system_biharmonic_coupled._assemble_function );
+
+      // initialize and solve the system
+      system.init();
+
+      system.MGsolve();
+
+      // convergence for u
+      std::pair< double , double > norm = GetErrorNorm_L2_H1_with_analytical_sol(&mlSol, "u", GetExactSolutionValue, GetExactSolutionGradient );
+      // // convergence for v
+      // std::pair< double , double > norm = GetErrorNorm_L2_H1_with_analytical_sol(&mlSol, "v", LaplaceGetExactSolutionValue, LaplaceGetExactSolutionGradient );
+
+
+      l2Norm[i][j]  = norm.first;
+      semiNorm[i][j] = norm.second;
+
+
+      // print solutions
+      std::vector < std::string > variablesToBePrinted;
+      variablesToBePrinted.push_back("All");
+
+      VTKWriter vtkIO(&mlSol);
+      vtkIO.Write(Files::_application_output_directory, "biquadratic", variablesToBePrinted, i);
+
+    }
+  }
+
+
+  // FE_convergence::output_convergence_order();
+
+
+  // ======= L2 - BEGIN  ========================
+  std::cout << std::endl;
+  std::cout << std::endl;
+  std::cout << "l2 ERROR and ORDER OF CONVERGENCE:\n\n";
+  std::cout << "LEVEL\tFIRST\t\t\tSERENDIPITY\t\tSECOND\n";
+
+  for (unsigned i = 0; i < maxNumberOfMeshes; i++) {
+    std::cout << i + 1 << "\t";
+    std::cout.precision(14);
+
+    for (unsigned j = 0; j < feOrder.size(); j++) {
+      std::cout << l2Norm[i][j] << "\t";
+    }
+
+    std::cout << std::endl;
+
+    if (i < maxNumberOfMeshes - 1) {
+      std::cout.precision(3);
+      std::cout << "\t\t";
+
+      for (unsigned j = 0; j < feOrder.size(); j++) {
+        std::cout << log(l2Norm[i][j] / l2Norm[i + 1][j]) / log(2.) << "\t\t\t";
+      }
+
+      std::cout << std::endl;
+    }
+
+  }
+  // ======= L2 - END  ========================
+
+
+
+  // ======= H1 - BEGIN  ========================
+
+  std::cout << std::endl;
+  std::cout << std::endl;
+  std::cout << "SEMINORM ERROR and ORDER OF CONVERGENCE:\n\n";
+  std::cout << "LEVEL\tFIRST\t\t\tSERENDIPITY\t\tSECOND\n";
+
+  for (unsigned i = 0; i < maxNumberOfMeshes; i++) {
+    std::cout << i + 1 << "\t";
+    std::cout.precision(14);
+
+    for (unsigned j = 0; j < feOrder.size(); j++) {
+      std::cout << semiNorm[i][j] << "\t";
+    }
+
+    std::cout << std::endl;
+
+    if (i < maxNumberOfMeshes - 1) {
+      std::cout.precision(3);
+      std::cout << "\t\t";
+
+      for (unsigned j = 0; j < feOrder.size(); j++) {
+        std::cout << log(semiNorm[i][j] / semiNorm[i + 1][j]) / log(2.) << "\t\t\t";
+      }
+
+      std::cout << std::endl;
+    }
+
+  }
+
+  // ======= H1 - END  ========================
+
+
+  return 0;
+}
+
+
+
+
+
+
---
+++ applications/zzz_sandbox/Karthik/poisson/poisson.cpp
@@ -0,0 +1,357 @@
+/**
+ * Solve 
+ *     - \Delta u = 1
+*/
+
+// library includes
+#include "FemusInit.hpp"
+#include "MultiLevelSolution.hpp"
+#include "MultiLevelProblem.hpp"
+#include "VTKWriter.hpp"
+#include "NonLinearImplicitSystem.hpp"
+#include "NumericVector.hpp"
+#include "SparseMatrix.hpp"
+
+#include "CurrentElem.hpp"
+#include "ElemType_template.hpp"
+
+#include "Assemble_jacobian.hpp"
+#include "Assemble_unknown_jacres.hpp"
+
+#include "Solution_functions_over_domains_or_mesh_files.hpp"
+
+
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+
+// application includes
+
+#define LIBRARY_OR_USER   1 //0: library; 1: user
+
+#if LIBRARY_OR_USER == 0
+   #include "00_poisson_eqn_with_dirichlet_or_neumann_bc.hpp"
+   #define NAMESPACE_FOR_POISSON  femus
+#elif LIBRARY_OR_USER == 1
+   #include "00_poisson_eqn.hpp"
+   #define NAMESPACE_FOR_POISSON  karthik
+#endif
+
+
+
+
+
+using namespace femus;
+ 
+
+
+double InitialValueDS(const MultiLevelProblem * ml_prob, const std::vector < double >& x, const char name[]) {
+    
+  return 0.;
+  
+}
+
+
+
+
+// SEGMENT - BEGIN
+
+ 
+bool segment_dir_neu_fine__laplacian__bc(const MultiLevelProblem * ml_prob, const std::vector < double >& x, const char name[], double& value, const int face_name, const double time) {
+
+  bool dirichlet = false;
+  value = 0.;
+  
+  const double tolerance = 1.e-5;
+  
+ if (ml_prob->GetMLMesh()->GetDimension() == 1 )  {
+  
+  if (face_name == 1) {
+      dirichlet = true;
+        value = 0.; //Dirichlet value
+    }
+  else if (face_name == 2) {
+      dirichlet = false;
+        value = 1.; //Neumann value
+    }
+
+    
+ }
+ 
+ 
+  return dirichlet;
+  
+ }
+// SEGMENT - END
+
+
+
+
+// SQUARE - BEGIN
+bool square__laplacian__bc(const MultiLevelProblem * ml_prob, const std::vector < double >& x, const char name[], double& value, const int face_name, const double time) {
+
+  if (ml_prob->GetMLMesh()->GetDimension() != 2 )  abort();
+
+   bool dirichlet = false;
+  value = 0.;
+
+
+  if (face_name == 1) {
+       dirichlet = true;
+         value = 0.;
+      //dirichlet = false;
+      //value = -1. * x[1] * ( 1. - x[1]) ;
+  }
+  else if (face_name == 2) {
+      dirichlet = false;
+        value = 0.;
+      // dirichlet = false;
+       value = -1. * x[1] * ( 1. - x[1]) ;
+  }
+
+ else  if (face_name == 3) {
+      dirichlet = true;
+        value = 0.;
+      // dirichlet = false;
+      // value = -1. * x[0] * ( 1. - x[0]) ;
+  }
+  else if (face_name == 4) {
+      //dirichlet = false;
+      //  value = 0.;
+      value = -1. * x[0] * ( 1. - x[0]);
+       dirichlet = false;
+       // // // value = -M_PI * sin( M_PI * x[0] );
+//        dirichlet = false;
+//        value = -M_PI * sin( M_PI * x[0] ) - 0.2 * (5. * M_PI) * sin( 5. * M_PI * x[0] );
+//        dirichlet = false;
+//        value = -2. * M_PI * sin( 2. * M_PI * x[0] );
+
+  }
+
+
+
+   return dirichlet;
+
+}
+
+// SQUARE - END
+
+
+
+// CUBE - BEGIN
+bool cube__laplacian__bc(const MultiLevelProblem * ml_prob, const std::vector < double >& x, const char name[], double& value, const int face_name, const double time) {
+
+  if (ml_prob->GetMLMesh()->GetDimension() != 3 )  abort();
+
+   bool dirichlet = false;
+  value = 0.;
+
+
+  if (face_name == 1) {
+      dirichlet = true;
+        value = 0.;
+  }
+  else if (face_name == 2) {
+      dirichlet = true;
+        value = 0.;
+  }
+
+ else  if (face_name == 3) {
+      dirichlet = true;
+        value = 0.;
+  }
+  else if (face_name == 4) {
+      dirichlet = true;
+        value = 0.;
+  }
+ else  if (face_name == 5) {
+      dirichlet = true;
+        value = 0.;
+  }
+  else if (face_name == 6) {
+      dirichlet = true;
+        value = 0.;
+  }
+
+
+
+   return dirichlet;
+
+}
+
+// CUBE - END
+
+
+ 
+
+
+int main(int argc, char** args) {
+
+  // ======= Init ========================
+  FemusInit mpinit(argc, args, MPI_COMM_WORLD);
+
+  // ======= Problem ========================
+  MultiLevelProblem ml_prob;
+
+  // ======= Files - BEGIN  ========================
+  const bool use_output_time_folder = false;
+  const bool redirect_cout_to_file = false;
+  Files files; 
+        files.CheckIODirectories(use_output_time_folder);
+        files.RedirectCout(redirect_cout_to_file);
+
+  // ======= Problem, Files ========================
+  ml_prob.SetFilesHandler(&files);
+  // ======= Files - END  ========================
+
+  // ======= Problem, Quad Rule - BEGIN ========================
+  std::string fe_quad_rule("seventh");
+
+  ml_prob.SetQuadratureRuleAllGeomElems(fe_quad_rule);
+  ml_prob.set_all_abstract_fe_AD_or_not();
+  // ======= Problem, Quad Rule - END  ========================
+
+    // ======= App Specifics - BEGIN  ==================
+  system_specifics  app_segment;   //me
+
+  //segment_dir_neu_fine
+  app_segment._system_name = "Equation";
+  app_segment._assemble_function = NAMESPACE_FOR_POISSON :: poisson_equation::equation_with_dirichlet_or_neumann_bc<double, double>;
+  
+// // //   app_segment._mesh_files.push_back("Mesh_1_x_dir_neu.med");
+// // //   app_segment._boundary_conditions_types_and_values             = segment_dir_neu_fine__laplacian__bc;
+  
+  app_segment._mesh_files.push_back("Mesh_2_xy_boundaries_groups_4x4.med");
+  app_segment._boundary_conditions_types_and_values             = square__laplacian__bc;
+  
+  Domains::square_01by01::Function_Zero_on_boundary_1<>    app_square_function_zero_on_boundary_1;
+  app_segment._assemble_function_for_rhs        = & app_square_function_zero_on_boundary_1;
+  
+  ///@todo if this is not set, nothing should happen here
+
+  
+    // ======= App Specifics - END  ==================
+
+
+  // ======= Mesh, files - BEGIN  ==================   
+//    mesh_files.push_back("Mesh_2_xy_all_dir.med");
+//    mesh_files.push_back("Mesh_2_xy_boundaries_groups_4x4.med");
+//    mesh_files.push_back("Mesh_1_x_all_dir.med");
+//    mesh_files.push_back("Mesh_1_y_all_dir.med");
+//    mesh_files.push_back("Mesh_1_z_all_dir.med");
+//    mesh_files.push_back("Mesh_2_xz_all_dir.med");
+//    mesh_files.push_back("Mesh_2_yz_all_dir.med");
+//    mesh_files.push_back("Mesh_3_xyz_all_dir.med");
+//    mesh_files.push_back("dome_tri.med");
+//    mesh_files.push_back("dome_quad.med");
+//    mesh_files.push_back("disk_quad.med");
+//    mesh_files.push_back("disk_quad_45x.med");
+//    mesh_files.push_back("disk_quad_90x.med");
+//    mesh_files.push_back("disk_tri.med");
+//    mesh_files.push_back("disk_tri_45x.med");
+//    mesh_files.push_back("disk_tri_90x.med");
+  // ======= Mesh, files - END ==================
+
+
+
+ for (unsigned int m = 0; m < app_segment._mesh_files.size(); m++)  {
+   
+  // ======= Problem, App (every Problem has 1 App for now) ========================
+  ml_prob.set_app_specs_pointer(& app_segment);
+  
+  
+  // ======= Mesh - BEGIN  ==================
+  MultiLevelMesh ml_mesh;
+  // ======= Mesh - END  ==================
+
+  // ======= Mesh, Coarse reading - BEGIN ==================
+  double scalingFactor = 1.;
+ 
+  const bool read_groups = true; //with this being false, we don't read any group at all. Therefore, we cannot even read the boundary groups that specify what are the boundary faces, for the boundary conditions
+  const bool read_boundary_groups = true;
+  
+  std::string mesh_file_tot = "./input/" + app_segment._mesh_files[m];
+  
+  ml_mesh.ReadCoarseMesh(mesh_file_tot.c_str(), fe_quad_rule.c_str(), scalingFactor, read_groups, read_boundary_groups);
+//     ml_mesh.GenerateCoarseBoxMesh(2,0,0,0.,1.,0.,0.,0.,0.,EDGE3,fe_quad_rule.c_str());
+//     ml_mesh.GenerateCoarseBoxMesh(0,2,0,0.,0.,0.,1.,0.,0.,EDGE3,fe_quad_rule.c_str());
+  // ======= Mesh, Coarse reading - END ==================
+
+  // ======= Mesh: Refinement - BEGIN  ==================
+  unsigned numberOfUniformLevels = /*1*/6;
+  unsigned numberOfSelectiveLevels = 0/*0*/;
+  ml_mesh.RefineMesh(numberOfUniformLevels , numberOfUniformLevels + numberOfSelectiveLevels, NULL);
+  // ======= Mesh: Refinement - END  ==================
+
+  // ======= Mesh: Coarse erasing - BEGIN  ========================
+  ml_mesh.EraseCoarseLevels(numberOfUniformLevels + numberOfSelectiveLevels - 1);
+  ml_mesh.PrintInfo();
+  // ======= Mesh: Coarse erasing - END  ========================
+
+  
+  // ======= Solution - BEGIN ==================
+  MultiLevelSolution ml_sol(&ml_mesh);
+
+  ml_sol.SetWriter(VTK);
+  ml_sol.GetWriter()->SetDebugOutput(true);
+
+  // ======= Problem, Mesh and Solution  ==================
+  ml_prob.SetMultiLevelMeshAndSolution(& ml_sol);
+  // ======= Solution - END ==================
+
+  // ======= Solutions that are Unknowns - BEGIN ==================
+  // add variables to ml_sol
+  ml_sol.AddSolution("d_s", LAGRANGE, FIRST/*DISCONTINUOUS_POLYNOMIAL, ZERO*/);
+
+  // ======= Solution: Initial Conditions ==================
+  ml_sol.Initialize("All");    // initialize all variables to zero
+  ml_sol.Initialize("d_s", InitialValueDS, & ml_prob);
+
+  // ======= Solution: Boundary Conditions ==================
+  ml_sol.AttachSetBoundaryConditionFunction(app_segment._boundary_conditions_types_and_values);
+  ml_sol.GenerateBdc("d_s", "Steady",  & ml_prob);
+
+  // ======= Solutions that are Unknowns - END ==================
+
+  
+//   std::vector < std::vector < const elem_type_templ_base<double, double> *  > > elem_all = ml_prob.evaluate_all_fe<double, double>();
+  
+    // ======= Problem, System - BEGIN ========================
+  ml_prob.clear_systems();
+
+  NonLinearImplicitSystem& system = ml_prob.add_system < NonLinearImplicitSystem > (app_segment._system_name);
+  
+  system.SetDebugNonlinear(true);
+ 
+  system.AddSolutionToSystemPDE("d_s");
+ 
+  // attach the assembling function to system
+  system.SetAssembleFunction( app_segment._assemble_function );
+
+//   system.SetMaxNumberOfLinearIterations(2);
+  // initialize and solve the system
+  system.SetMgType(V_CYCLE/*F_CYCLE*//*M_CYCLE*/); //it doesn't matter if I use only 1 level
+
+
+  system.SetOuterSolver(GMRES);
+ 
+  system.init();
+  
+  system.MGsolve();
+    // ======= Problem, System - END ========================
+
+    // ======= Print - BEGIN ========================
+  const std::string print_order = fe_fams_for_files[ FILES_CONTINUOUS_BIQUADRATIC ];
+  std::vector < std::string > variablesToBePrinted;
+  variablesToBePrinted.push_back("all");
+ 
+  ml_sol.GetWriter()->Write(app_segment._mesh_files[m], files.GetOutputPath(), print_order.c_str(), variablesToBePrinted);
+    // ======= Print - END ========================
+
+  }  //end mesh file loop
+ 
+ 
+  return 0;
+}
+
+
